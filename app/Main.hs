b31385c82b7effe887130e3a72b4f224f5af7cb8
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-05-13 09:22:01 +0200

    Implementation of Rule2 and Rule3
    Rule2: Check if all Func-Vars final
    Rule3: Check if Class-Vars, which are only representet by A-Z, _ and 0-9 are final

--- app/Main.hs
+++ app/Main.hs
@@ -48,3 +48,56 @@ buildAst path pretty =
       Left error -> print error
       Right cUnit -> do
         if pretty then print (prettyPrint cUnit) else print cUnit
+        checkCU cUnit
+
+checkCU (CompilationUnit a b c) = do
+  print (typeDeclFunc c)
+
+arrayContains :: [Modifier] -> String -> Bool
+arrayContains [] sWord = False
+arrayContains (x : xs) sWord = if (show x) == sWord then True else arrayContains xs sWord
+
+data Errors = ClassVarNotFinal {var :: String} | FuncVarNotFinal {func :: String, var :: String}
+  deriving (Show)
+
+typeDeclFunc :: [TypeDecl] -> [Errors]
+typeDeclFunc [] = []
+typeDeclFunc (item : restList) =
+  case item of
+    ClassTypeDecl a -> classTypeDeclFunc a ++ typeDeclFunc restList
+    InterfaceTypeDecl a -> []
+
+classTypeDeclFunc item =
+  case item of
+    ClassDecl modifier (Ident ident) typeParam mRefType refType (ClassBody body) -> classBodyFunc body
+    EnumDecl modifier (Ident ident) refType body -> []
+
+classBodyFunc [] = []
+classBodyFunc (x : xs) =
+  case x of
+    MemberDecl memberDecl -> memberDeclFunc memberDecl ++ classBodyFunc xs
+    InitDecl bool block -> []
+
+memberDeclFunc item =
+  case item of
+    FieldDecl modifier typeParam ((VarDecl (VarId (Ident n)) x) : xs) -> ruleThree modifier n
+    MethodDecl modifier typeParam mType (Ident ident) formalParam exceptionType mExp methodBody -> formalParamFunc formalParam ident
+    _ -> []
+
+formalParamFunc :: [FormalParam] -> String -> [Errors]
+formalParamFunc [] fName = []
+formalParamFunc ((FormalParam modifier jType bool (VarId (Ident n))) : xs) fName = ruleTwo modifier fName n ++ (formalParamFunc xs fName)
+
+ruleTwo :: [Modifier] -> String -> String -> [Errors]
+ruleTwo [] fName vName = [FuncVarNotFinal {func = fName, var = vName}]
+ruleTwo modifier fName vName = if arrayContains modifier "final" then [] else [FuncVarNotFinal {func = fName, var = vName}]
+
+ruleThree :: [Modifier] -> String -> [Errors]
+ruleThree [] vName = if checkUpperLowbarNumber vName then [ClassVarNotFinal {var = vName}] else []
+ruleThree modifier vName = if not (arrayContains modifier "final") && checkUpperLowbarNumber vName then [ClassVarNotFinal {var = vName}] else []
+
+checkUpperLowbarNumber :: String -> Bool
+checkUpperLowbarNumber vName = g vName
+  where
+    g [] = True
+    g (c : restList) = if c `elem` ['A' .. 'Z'] || c `elem` "_" || c `elem` ['0' .. '9'] then g restList else False
--- test/Strings2.java
+++ test/Strings2.java
@@ -1,12 +1,12 @@
 package de.fuas.algorithms;
 
 public class Strings2 {
-    static int ANZ_ASCII_ZEICHEN = 128;
+    static int ANZ_ASCII_ZEICHEN_1 = 128;
 
     final int count = 0;
     String name = "Piet";
 
-    public void testFunc() {
+    public void testFunc(String name, final int age) {
         System.out.println("Hallo, hier wird geprintet");
     }
 }
