413cf2994ebfe64e6a09029551c5c2ee9e19340f
Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2022-05-18 14:21:12 +0200

    Extract Check Funktions to new module, refactor functions based on review annotations

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import Options.Applicative
 import Data.Semigroup((<>))
 import Language.Java.Parser (parser, compilationUnit, modifier)
 import Language.Java.Pretty(prettyPrint, pretty)
-import Language.Java.Syntax
+import CheckNonPrivateAttributes(check)
 
 main :: IO ()
 main = importJava =<< execParser opts
@@ -17,7 +17,7 @@ main = importJava =<< execParser opts
 
 
 importJava :: Params -> IO ()
-importJava (Params path prettie) = buildAst path prettie
+importJava (Params path prettie) = parseJava path prettie
 
 data Params = Params
   {
@@ -35,8 +35,8 @@ params = Params
           ( long "prettie"
           <> help "By setting these Param the AST is showing after the Prettier")
 
-buildAst :: FilePath -> Bool -> IO ()
-buildAst path pretty =
+parseJava :: FilePath -> Bool -> IO ()
+parseJava path pretty =
   do
     input <- readFile path
     let result = parser compilationUnit input
@@ -45,47 +45,4 @@ buildAst path pretty =
         print error
       Right cUnit ->
         -- if pretty then writeFile "./ast.txt" (prettyPrint cUnit) else print cUnit
-        check cUnit
-
-check :: CompilationUnit -> IO ()
-check (CompilationUnit _ _ classtype) =
-    do
-      print (checkType classtype)
-
-data Errors = ClassVarNotPrivate {var :: String}
-  deriving (Show)
-
-checkType :: [TypeDecl] -> [Errors]
-checkType [] = []
-checkType (typedecl:restlist) = case typedecl of
-  ClassTypeDecl cd -> checkClassType cd ++ checkType restlist
-  InterfaceTypeDecl id -> []
-
-
-checkClassType :: ClassDecl -> [Errors]
-checkClassType node =
-    case node of
-      ClassDecl _ _ _ _ _ (ClassBody body) ->
-        checkDecl body
-      EnumDecl {} -> []
-
-
-checkDecl :: [Decl] -> [Errors]
-checkDecl [] = []
-checkDecl (member:restlist) = case member of
-  MemberDecl md -> checkMemberDecl md ++ checkDecl restlist
-  InitDecl b bl -> []
-
-checkMemberDecl :: MemberDecl -> [Errors]
-checkMemberDecl member = 
-  case member of
-    FieldDecl mods ty ((VarDecl (VarId (Ident n)) x) : xs) -> checkFieldDecl mods n
-    MethodDecl mods tps m_ty id fps rts m_exp mb -> []
-    ConstructorDecl mods tps id fps rts cb -> []
-    MemberClassDecl cd -> []
-    MemberInterfaceDecl id -> []
-
-
-checkFieldDecl :: [Modifier] -> String -> [Errors]
-checkFieldDecl [] varname = [ClassVarNotPrivate {var = varname}]
-checkFieldDecl modifier varname = [ClassVarNotPrivate {var = varname} | Private `notElem` modifier]
+       print (check cUnit)
