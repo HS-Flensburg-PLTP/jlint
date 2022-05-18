754b8c6eb4372236b645060f816631e9131dbec4
Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2022-05-18 08:56:12 +0200

    complete first linter rule - put all not private attributes into List of Errors and print it to console

--- app/Main.hs
+++ app/Main.hs
@@ -27,108 +27,65 @@ data Params = Params
 
 params :: Parser Params
 params = Params
-      <$> strOption 
+      <$> strOption
           ( long "path"
           <> metavar "SRCPath"
           <> help "Path to the Java-File")
-      <*> switch 
+      <*> switch
           ( long "prettie"
           <> help "By setting these Param the AST is showing after the Prettier")
 
 buildAst :: FilePath -> Bool -> IO ()
-buildAst path pretty = 
-  do 
+buildAst path pretty =
+  do
     input <- readFile path
     let result = parser compilationUnit input
-    case result of 
-      Left error -> 
+    case result of
+      Left error ->
         print error
-      Right cUnit -> 
+      Right cUnit ->
         -- if pretty then writeFile "./ast.txt" (prettyPrint cUnit) else print cUnit
         check cUnit
 
 check :: CompilationUnit -> IO ()
-check unit =
-    case unit of
-        CompilationUnit _ _ classtype->
-            checkType classtype
+check (CompilationUnit _ _ classtype) =
+    do
+      print (checkType classtype)
 
-checkType :: [TypeDecl] -> IO ()
-checkType list =
-  case list of
-    (node:remaininglist) ->
-      case node of
-        ClassTypeDecl a ->
-          do
-            checkClassType a
-            checkType remaininglist
-        InterfaceTypeDecl a ->
-            print "Interface"
-    [] ->
-      print "Empty Node"
+data Errors = ClassVarNotPrivate {var :: String}
+  deriving (Show)
 
-checkClassType :: ClassDecl -> IO ()
+checkType :: [TypeDecl] -> [Errors]
+checkType [] = []
+checkType (typedecl:restlist) = case typedecl of
+  ClassTypeDecl cd -> checkClassType cd ++ checkType restlist
+  InterfaceTypeDecl id -> []
+
+
+checkClassType :: ClassDecl -> [Errors]
 checkClassType node =
     case node of
-      ClassDecl _ _ _ _ _ i ->
-        checkClassBody i
-      EnumDecl {} ->
-        print "Enum"
-      
-checkClassBody :: ClassBody -> IO ()
-checkClassBody node =
-    case node of 
-        ClassBody d ->
-          checkDecl d   
-
-checkDecl :: [Decl] -> IO ()
-checkDecl list =
-  case list of
-    (node:remaininlist) ->
-      case node of
-        MemberDecl member ->
-          do
-            checkMemberDecl member
-            checkDecl remaininlist
-        InitDecl _ _ -> 
-          print "InitDecl"
-    [] ->
-      print "Empty Node"
-      
-checkMemberDecl :: MemberDecl -> IO ()
-checkMemberDecl member =
-    case member of
-      FieldDecl m t v ->
-        checkFieldDecl m t v
-      MethodDecl {} ->
-        print "Method"
-      ConstructorDecl {} ->
-        print "Constructor"
-      MemberClassDecl _ ->
-        print "MemberClass"
-      MemberInterfaceDecl _ ->
-        print "MemberInterface"
+      ClassDecl _ _ _ _ _ (ClassBody body) ->
+        checkDecl body
+      EnumDecl {} -> []
 
 
-checkFieldDecl modifier vartype varname =
-  case modifier of
-    [] ->
-      print "No Modifier"
-    (x:xs) ->
-      if x == Public then
-        do
-          print x
-          print varname
-          checkFieldDecl xs vartype varname
-      else
-        checkFieldDecl xs vartype varname
+checkDecl :: [Decl] -> [Errors]
+checkDecl [] = []
+checkDecl (member:restlist) = case member of
+  MemberDecl md -> checkMemberDecl md ++ checkDecl restlist
+  InitDecl b bl -> []
 
-{-
-@Lukas
-Der Itteriert nun komplett durch die Struktur bis zur Fielddeclaration
-bisher sind alle Funktionen eher IO Funktionen und er gibt jeweils nur die Felder aus.
-In der Fielddeclaration printet er nun alle public Elemente jeweils aus.
-Als nächste Schritte müsste dann von "unten" also von der FieldDecl hoch das in eine Liste verschachteln in der halt
-alle Variablen sind. Entweder dann oder davor muss natürlich noch nach dem Modifier public gefiltert werden.
-Bisher ist für jede "Ebene" eine eigene Funktion geschrieben.
--}
+checkMemberDecl :: MemberDecl -> [Errors]
+checkMemberDecl member = 
+  case member of
+    FieldDecl mods ty ((VarDecl (VarId (Ident n)) x) : xs) -> checkFieldDecl mods n
+    MethodDecl mods tps m_ty id fps rts m_exp mb -> []
+    ConstructorDecl mods tps id fps rts cb -> []
+    MemberClassDecl cd -> []
+    MemberInterfaceDecl id -> []
+
+
+checkFieldDecl :: [Modifier] -> String -> [Errors]
+checkFieldDecl [] varname = [ClassVarNotPrivate {var = varname}]
+checkFieldDecl modifier varname = [ClassVarNotPrivate {var = varname} | Private `notElem` modifier]
