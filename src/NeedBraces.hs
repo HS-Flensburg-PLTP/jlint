b411bcbca2128fa98626154a58981c00a953c7b5
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-06-14 13:51:38 +0200

    Restructure complete

--- src/NeedBraces.hs
+++ src/NeedBraces.hs
@@ -4,61 +4,45 @@
 
 module NeedBraces where
 
+import Control.Monad (MonadPlus (..))
 import Data.Generics.Uniplate.Data (Biplate, universeBi)
 import Language.Java.Syntax
-import RDF (simpleDiagnostic, Diagnostic(..))
+import RDF (Diagnostic (..), simpleDiagnostic)
 
 check :: CompilationUnit -> FilePath -> [Diagnostic]
-check cUnit path = 
-    let 
-        fBlocks = grabFunc cUnit
+check cUnit path =
+  let fBlocks = extractMethods cUnit
 
-        search[] = []
-        search ((b,n):xs) = 
-            (checkGeneral (grabDoBlockStmt b) n path "A Do-Part") ++ 
-            (checkGeneral (grabWhileBlockStmt b) n path "A While-Part") ++ 
-            (checkGeneral (grabForBlockStmt b) n path "A For-Part") ++
-            (checkGeneral (grabIfThenElseBlockStmt b) n path "A IfThenElse-Part") ++
-            (checkGeneral (grabIfThenBlockStmt b) n path "A IfThen-Part") ++
-            (checkIfThenElseLast (grabIfThenElseLastBlockStmt b) n path) ++
-            search xs
-    in
-        search fBlocks
+      search [] = []
+      search ((n, b) : xs) = extractStatements b n path
+   in search fBlocks
 
-grabFunc :: Biplate CompilationUnit MemberDecl => CompilationUnit -> [(MethodBody , String)]
-grabFunc cUnit = [(b, n) | MethodDecl _ _ _ (Ident n) _ _ _ b <- universeBi cUnit ]
+extractMethods :: CompilationUnit -> [(String, MethodBody)]
+extractMethods cUnit = do
+  membDecl <- universeBi cUnit
+  extractBody membDecl
+  where
+    extractBody (MethodDecl _ _ _ (Ident n) _ _ _ b) = return (n, b)
+    extractBody _ = mzero
 
-grabDoBlockStmt :: Biplate MethodBody BlockStmt => MethodBody -> [Stmt]
-grabDoBlockStmt b = [y | Do y _ <- universeBi b]
-
-grabWhileBlockStmt :: Biplate MethodBody BlockStmt => MethodBody -> [Stmt]
-grabWhileBlockStmt b = [y | While _ y <- universeBi b]
-
-grabForBlockStmt :: Biplate MethodBody BlockStmt => MethodBody -> [Stmt]
-grabForBlockStmt b = [y | BasicFor _ _ _ y <- universeBi b]
-
-grabIfThenBlockStmt :: Biplate MethodBody BlockStmt => MethodBody -> [Stmt]
-grabIfThenBlockStmt b = [y | IfThen _ y <- universeBi b]
-
-grabIfThenElseBlockStmt :: Biplate MethodBody BlockStmt => MethodBody -> [Stmt]
-grabIfThenElseBlockStmt b = [y | IfThenElse _ y _ <- universeBi b ]
-
-grabIfThenElseLastBlockStmt :: Biplate MethodBody BlockStmt => MethodBody -> [Stmt]
-grabIfThenElseLastBlockStmt b = [y | IfThenElse _ _ y <- universeBi b ]
+extractStatements :: MethodBody -> String -> FilePath -> [Diagnostic]
+extractStatements methodBody funcName path = do
+  stmt <- universeBi methodBody
+  extractStatement stmt
+  where
+    extractStatement (Do (StmtBlock _) _) = mzero
+    extractStatement (Do _ _) = return (simpleDiagnostic (msg "A Do-Part" funcName) path)
+    extractStatement (While _ (StmtBlock _)) = mzero
+    extractStatement (While _ _) = return (simpleDiagnostic (msg "A While-Part" funcName) path)
+    extractStatement (BasicFor _ _ _ (StmtBlock _)) = mzero
+    extractStatement (BasicFor _ _ _ _) = return (simpleDiagnostic (msg "A For-Part" funcName) path)
+    extractStatement (IfThen _ (StmtBlock _)) = mzero
+    extractStatement (IfThen _ _) = return (simpleDiagnostic (msg "A IfThen-Part" funcName) path)
+    extractStatement (IfThenElse _ (StmtBlock _) (StmtBlock _)) = mzero
+    extractStatement (IfThenElse _ _ (StmtBlock _)) = return (simpleDiagnostic (msg "A IfThenElse-Part" funcName) path)
+    extractStatement (IfThenElse _ (StmtBlock _) _) = return (simpleDiagnostic (msg "A IfThenElse-Part" funcName) path)
+    extractStatement (IfThenElse _ _ _) = return (simpleDiagnostic (msg "A IfThenElse-Part" funcName) path)
+    extractStatement _ = mzero
 
 msg :: String -> String -> String
-msg t funcName = t ++ " in function " ++ funcName ++ " contains no Braces."
-
-checkGeneral :: [Stmt] -> String -> FilePath -> String -> [Diagnostic]
-checkGeneral [] _  _ _= []
-checkGeneral ((StmtBlock _):xs) funcName  path msgBegin = [] ++ checkGeneral xs funcName path msgBegin
-checkGeneral (_:xs) funcName path msgBegin= [simpleDiagnostic (msg msgBegin funcName) path] ++ checkGeneral xs funcName path msgBegin
-
-checkIfThenElseLast :: [Stmt] -> String -> FilePath -> [Diagnostic]
-checkIfThenElseLast [] _ _ = []
-checkIfThenElseLast ((StmtBlock _) : xs) n path = [] ++ checkIfThenElseLast xs n path
-checkIfThenElseLast ((IfThenElse _ _ _) : xs) n path = [] ++ checkIfThenElseLast xs n path
-checkIfThenElseLast (_:xs) n  path = [simpleDiagnostic (msg "A IfThenElse-Part" n) path] ++ checkIfThenElseLast xs n path
-
-    
-     
+msg t funcName = t ++ " in function " ++ funcName ++ " contains no braces."
