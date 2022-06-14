fe6d663527c906b077386fbe358a2828f616e5e9
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-06-14 14:48:05 +0200

    refactor check, rename methods, create AST.hs
    AST contains now the `extractKMethods`-Method, which is needed in multiple Rules

--- src/NeedBraces.hs
+++ src/NeedBraces.hs
@@ -4,45 +4,34 @@
 
 module NeedBraces where
 
+import AST (extractMethods)
 import Control.Monad (MonadPlus (..))
 import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.Syntax
 import RDF (Diagnostic (..), simpleDiagnostic)
 
 check :: CompilationUnit -> FilePath -> [Diagnostic]
-check cUnit path =
-  let fBlocks = extractMethods cUnit
+check cUnit path = do
+  methods <- extractMethods cUnit
+  extractDiagnostics methods path
 
-      search [] = []
-      search ((n, b) : xs) = extractStatements b n path ++ search xs
-   in search fBlocks
-
-extractMethods :: CompilationUnit -> [(String, MethodBody)]
-extractMethods cUnit = do
-  membDecl <- universeBi cUnit
-  extractBody membDecl
-  where
-    extractBody (MethodDecl _ _ _ (Ident n) _ _ _ b) = return (n, b)
-    extractBody _ = mzero
-
-extractStatements :: MethodBody -> String -> FilePath -> [Diagnostic]
-extractStatements methodBody funcName path = do
+extractDiagnostics :: (String, MethodBody) -> FilePath -> [Diagnostic]
+extractDiagnostics (funcName, methodBody) path = do
   stmt <- universeBi methodBody
-  extractStatement stmt
+  extractDiagnostic stmt
   where
-    extractStatement (Do (StmtBlock _) _) = mzero
-    extractStatement (Do _ _) = return (simpleDiagnostic (msg "A Do-Part" funcName) path)
-    extractStatement (While _ (StmtBlock _)) = mzero
-    extractStatement (While _ _) = return (simpleDiagnostic (msg "A While-Part" funcName) path)
-    extractStatement (BasicFor _ _ _ (StmtBlock _)) = mzero
-    extractStatement (BasicFor _ _ _ _) = return (simpleDiagnostic (msg "A For-Part" funcName) path)
-    extractStatement (IfThen _ (StmtBlock _)) = mzero
-    extractStatement (IfThen _ _) = return (simpleDiagnostic (msg "A IfThen-Part" funcName) path)
-    extractStatement (IfThenElse _ (StmtBlock _) (StmtBlock _)) = mzero
-    extractStatement (IfThenElse _ _ (StmtBlock _)) = return (simpleDiagnostic (msg "A IfThenElse-Part" funcName) path)
-    extractStatement (IfThenElse _ (StmtBlock _) _) = return (simpleDiagnostic (msg "A IfThenElse-Part" funcName) path)
-    extractStatement (IfThenElse _ _ _) = return (simpleDiagnostic (msg "A IfThenElse-Part" funcName) path)
-    extractStatement _ = mzero
+    extractDiagnostic (Do (StmtBlock _) _) = mzero
+    extractDiagnostic (Do _ _) = return (simpleDiagnostic (msg "A Do-Part" funcName) path)
+    extractDiagnostic (While _ (StmtBlock _)) = mzero
+    extractDiagnostic (While _ _) = return (simpleDiagnostic (msg "A While-Part" funcName) path)
+    extractDiagnostic (BasicFor _ _ _ (StmtBlock _)) = mzero
+    extractDiagnostic (BasicFor _ _ _ _) = return (simpleDiagnostic (msg "A For-Part" funcName) path)
+    extractDiagnostic (IfThen _ (StmtBlock _)) = mzero
+    extractDiagnostic (IfThen _ _) = return (simpleDiagnostic (msg "A IfThen-Part" funcName) path)
+    extractDiagnostic (IfThenElse _ (StmtBlock _) (StmtBlock _)) = mzero
+    extractDiagnostic (IfThenElse _ _ (StmtBlock _)) = return (simpleDiagnostic (msg "A IfThenElse-Part" funcName) path)
+    extractDiagnostic (IfThenElse _ (StmtBlock _) _) = return (simpleDiagnostic (msg "A IfThenElse-Part" funcName) path)
+    extractDiagnostic (IfThenElse _ _ _) = return (simpleDiagnostic (msg "A IfThenElse-Part" funcName) path)
+    extractDiagnostic _ = mzero
 
-msg :: String -> String -> String
-msg t funcName = t ++ " in function " ++ funcName ++ " contains no braces."
+    msg t funcName = t ++ " in function " ++ funcName ++ " contains no braces."
