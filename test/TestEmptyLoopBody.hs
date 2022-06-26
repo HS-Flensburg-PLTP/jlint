3ab908f619dc7affe668e02ac5def6bc50b2d912
Lukas Nolde <lukas@medimad.de>
2022-06-26 16:39:02 +0200

    Start implemnting bracket - pattern to generalize test setup and teardown

--- test/TestEmptyLoopBody.hs
+++ test/TestEmptyLoopBody.hs
@@ -1,11 +1,36 @@
-module TestEmptyLoopBody where
 
+module TestEmptyLoopBody where 
+import Test.HUnit
 import Language.Java.Parser (compilationUnit, parser)
+import EmptyLoopBody(check)
 import Language.Java.Syntax
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+import UnliftIO.Exception
+import System.Directory
 
-parseJava :: String -> Maybe Language.Java.Syntax.CompilationUnit
-parseJava input = do
-  let result = parser compilationUnit input
-  case result of
-    Left _ -> Nothing
-    Right cUnit -> Just cUnit
+before :: IO CompilationUnit
+before = do
+    path <- getCurrentDirectory 
+    parseJava (path ++ "/test/Strings2.java")
+
+
+parseJava :: String -> IO CompilationUnit
+parseJava path = do
+    input <- readFile path
+    let result  = parser compilationUnit input
+    case result of 
+        Left error -> throwString (show error)
+        Right cUnit -> return cUnit
+
+testEmptyDoLoop :: IO()
+testEmptyDoLoop = do
+    cUnit <- before 
+    print cUnit
+
+-- testEmptyDoLoop = TestCase( do 
+    -- cUnit <- parseJava "while(++i < --j);"
+    -- let (x : xs) = EmptyLoopBody.check cUnit ""
+    -- print x
+    -- assert(return ""))
+    -- assertEqual "tesmsg" "A While-Loop has a empty loop body." x.message )
