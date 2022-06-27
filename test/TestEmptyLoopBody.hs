6c38fd9229e06f901faf89ededeaf7be45e96180
Lukas Nolde <lukas@medimad.de>
2022-06-27 02:16:19 +0200

    Extract cUnit setup into RessourceManager.hs

--- test/TestEmptyLoopBody.hs
+++ test/TestEmptyLoopBody.hs
@@ -1,59 +1,21 @@
 
 module TestEmptyLoopBody where 
 import Test.HUnit
-import Language.Java.Parser (compilationUnit, parser)
 import EmptyLoopBody(check)
-import Language.Java.Syntax
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-import UnliftIO.Exception
-import System.Directory
-import Control.Monad.Reader ( runReader, MonadReader(ask), Reader )
+import RessourceManager
 
-
-before :: FilePath -> IO (FilePath, CompilationUnit)
-before pathFromRoot = do
-    path <- getCurrentDirectory 
-    cUnit<- parseJava (path ++ pathFromRoot)
-    return (path, cUnit)
-
-
-parseJava :: String -> IO CompilationUnit
-parseJava path = do
-    input <- readFile path
-    let result  = parser compilationUnit input
-    case result of 
-        Left error -> throwString (show error)
-        Right cUnit -> return cUnit
-
-after :: (FilePath, CompilationUnit) -> IO ()
-after (fPath, cUnit) =
-    -- implement teardown here
-    return ()
-
-withCUnit :: String -> ((FilePath, CompilationUnit) -> IO ()) -> IO ()
-withCUnit fPath =
-    bracket ((before fPath)) after -- bracket before after during
+-- {-# LANGUAGE NoImplicitPrelude #-}
+-- {-# LANGUAGE OverloadedStrings #-}
 
 testEmptyDoLoop :: IO()
 testEmptyDoLoop = do
-    runTestTT test1
+    runTestTT testEmptyDoLoob
     return ()
-    -- cUnit <- before 
-    -- print cUnit
 
-test1 = TestList [
+testEmptyDoLoob = TestList [
     "Empty Do Loop" ~: withCUnit "/test/Strings2.java" (\(path, cUnit) -> do
             let actual = EmptyLoopBody.check cUnit path
             let expected = EmptyLoopBody.check cUnit path
             False @? "Empty do Loop does not match"
         )
     ]
-
-
--- testEmptyDoLoop = TestCase( do 
-    -- cUnit <- parseJava "while(++i < --j);"
-    -- let (x : xs) = EmptyLoopBody.check cUnit ""
-    -- print x
-    -- assert(return ""))
-    -- assertEqual "tesmsg" "A While-Loop has a empty loop body." x.message )
