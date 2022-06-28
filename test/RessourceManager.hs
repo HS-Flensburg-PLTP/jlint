3decc3bf5e6137fd54050f7f5388a42cc6e0bff2
Lukas Nolde <lukas@medimad.de>
2022-06-28 02:18:33 +0200

    Add first properly working test testEmtpyLoopbody

---
+++ test/RessourceManager.hs
@@ -0,0 +1,29 @@
+module RessourceManager where
+
+import Language.Java.Parser (compilationUnit, parser)
+import Language.Java.Syntax
+import UnliftIO.Exception
+import System.Directory
+
+-- parse Java if fail 
+parseJava :: String -> IO CompilationUnit
+parseJava path = do
+    input <- readFile path
+    let result  = parser compilationUnit input
+    case result of 
+        Left error -> throwString (show error) -- throwString :: (MonadIO m, HasCallStack) => String -> m a
+        Right cUnit -> return cUnit
+
+
+withCUnit :: String -> ((FilePath, CompilationUnit) -> IO ()) -> IO ()
+withCUnit fPath =
+    bracket ((setupCUnit fPath)) teardownCunit -- bracket setupCUnit after during
+    where
+        setupCUnit :: FilePath -> IO (FilePath, CompilationUnit)
+        setupCUnit pathFromRoot = do
+            path <- getCurrentDirectory 
+            cUnit <- parseJava (path ++ pathFromRoot)
+            return (path, cUnit)
+
+        teardownCunit :: (FilePath, CompilationUnit) -> IO ()
+        teardownCunit _ = return ()
