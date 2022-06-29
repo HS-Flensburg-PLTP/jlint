8eaf2094a797bc4f7325bfa9d9bee94151469a11
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-06-29 07:42:17 +0200

    formatting files with ormolu

--- src/CheckNonFinalMethodAttributes.hs
+++ src/CheckNonFinalMethodAttributes.hs
@@ -1,10 +1,9 @@
 module CheckNonFinalMethodAttributes (check) where
 
+import Control.Monad.Extra (concatMapM)
+import Control.Monad.Reader (MonadReader (ask), Reader, runReader)
 import Language.Java.Syntax (ClassBody (..), ClassDecl (..), CompilationUnit (..), Decl (..), FormalParam (..), Ident (Ident), MemberDecl (..), Modifier (Final), TypeDecl (..), VarDeclId (VarId))
 import RDF (Diagnostic (..), Location (..))
-import Control.Monad.Reader ( runReader, MonadReader(ask), Reader )
-import Control.Monad.Extra ( concatMapM )
-
 
 check :: CompilationUnit -> FilePath -> [Diagnostic]
 check (CompilationUnit _ _ classtype) = runReader (checkTypeDecls classtype)
@@ -33,28 +32,28 @@ checkMemberDecl _ = return []
 
 checkMethodDecl :: String -> [FormalParam] -> Reader FilePath [Diagnostic]
 checkMethodDecl _ [] = return []
-checkMethodDecl ident (x:xs) = do
-    cfp <- checkFormalParam x ident
-    cmd <- checkMethodDecl ident xs
-    return (cfp ++ cmd)
+checkMethodDecl ident (x : xs) = do
+  cfp <- checkFormalParam x ident
+  cmd <- checkMethodDecl ident xs
+  return (cfp ++ cmd)
 
 checkFormalParam :: FormalParam -> String -> Reader FilePath [Diagnostic]
 checkFormalParam (FormalParam modifier _ _ (VarId (Ident n))) ident = do
-      path <- ask
-      return [constructDiagnostic n ident path | Final `notElem` modifier ]
+  path <- ask
+  return [constructDiagnostic n ident path | Final `notElem` modifier]
 
 constructDiagnostic :: String -> String -> FilePath -> Diagnostic
 constructDiagnostic fparam ident path =
   Diagnostic
-      { message = "Argument " ++ fparam ++ " in " ++ ident ++ " is not declared as 'final'.",
-        location =
-          Location
-            { path = path,
-              locationRange = Nothing
-            },
-        severity = Just (Left "WARNING"),
-        source = Nothing,
-        code = Nothing,
-        suggestions = Nothing,
-        originalOutput = Nothing
-      }
+    { message = "Argument " ++ fparam ++ " in " ++ ident ++ " is not declared as 'final'.",
+      location =
+        Location
+          { path = path,
+            locationRange = Nothing
+          },
+      severity = Just (Left "WARNING"),
+      source = Nothing,
+      code = Nothing,
+      suggestions = Nothing,
+      originalOutput = Nothing
+    }
