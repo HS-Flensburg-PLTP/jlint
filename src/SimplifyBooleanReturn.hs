8f95a6392714d99d1ceef31b9013b5c1e71c9ff8
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-01 09:47:11 +0200

    change msg, use guard, use recusion

--- src/SimplifyBooleanReturn.hs
+++ src/SimplifyBooleanReturn.hs
@@ -19,10 +19,14 @@ checkStatements (methodName, methodBody) path = do
   stmt <- universeBi methodBody
   checkStatement stmt
   where
-    checkStatement (IfThenElse _ a b) = if isReturnBool a && isReturnBool b then return (methodDiagnostic methodName "A if-then-else part with boolean return can be simplified." path) else mzero
-    checkStatement (IfThen _ a) = if isReturnBool a then return (methodDiagnostic methodName "A if-then part with boolean return can be simplified." path) else mzero
+    checkStatement (IfThenElse _ a b)
+      | isReturnBool a && isReturnBool b = return (methodDiagnostic methodName "A if-then-else part with literal return can be simplified." path)
+      | otherwise = mzero
+    checkStatement (IfThen _ a)
+      | isReturnBool a = return (methodDiagnostic methodName "A if-then part with literal return can be simplified." path)
+      | otherwise = mzero
     checkStatement _ = mzero
 
     isReturnBool (Return (Just (Lit (Boolean _)))) = True
-    isReturnBool (StmtBlock (Block [BlockStmt (Return (Just (Lit (Boolean _))))])) = True
+    isReturnBool (StmtBlock (Block [BlockStmt a])) = isReturnBool a
     isReturnBool _ = False
