afee24adcc19c271bd742422ba8013875d66e7f0
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-11 11:50:59 +0200

    unused LocalVar check implementet

--- src/UnusedLocalVariable.hs
+++ src/UnusedLocalVariable.hs
@@ -5,10 +5,10 @@ import Control.Monad (MonadPlus (..))
 import Data.Function ((&))
 import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.Syntax
-import RDF (Diagnostic (..), methodDiagnostic)
+import RDF (Diagnostic (..), methodDiagnostic, simpleDiagnostic)
 
-check :: CompilationUnit -> FilePath -> [Diagnostic]
-check cUnit path = do
+checkMethodVars :: CompilationUnit -> FilePath -> [Diagnostic]
+checkMethodVars cUnit path = do
   (methodName, methodBody) <- extractMethods cUnit
   checkIfUsed (extractMethodVariables methodBody) (extractMethodVariableUsages methodBody) methodName path
 
@@ -26,10 +26,33 @@ extractMethodVariableUsages methodBody = do
   extractUsedVariables usedVariables
   where
     extractUsedVariables (Name varList) = map (\(Ident n) -> n) varList
-    extractUsedVariables _ = mzero
 
 checkIfUsed :: [String] -> [String] -> String -> FilePath -> [Diagnostic]
 checkIfUsed declaredVars usedVars methodName path =
   declaredVars
     & filter (`notElem` usedVars)
     & map (\var -> methodDiagnostic methodName ("Variable " ++ var ++ " is declared but never used.") path)
+
+checkAllVars :: CompilationUnit -> FilePath -> [Diagnostic]
+checkAllVars cUnit path =
+  concatMap (\var -> if test (extractMethods cUnit) var then mzero else [simpleDiagnostic ("LocalVariable " ++ var ++ " is declared but never used.") path]) (extractClassVars cUnit)
+
+test :: [(String, MethodBody)] -> String -> Bool
+test methods var =
+  methods
+    & any
+      ( \(_, body) ->
+          checkelDiCheck var (extractMethodVariables body) (extractMethodVariableUsages body)
+      )
+
+extractClassVars :: CompilationUnit -> [String]
+extractClassVars cUnit = do
+  vars <- universeBi cUnit
+  extractVars vars
+  where
+    extractVars (FieldDecl _ _ varDecls) = map (\(VarDecl (VarId (Ident n)) _) -> n) varDecls
+    extractVars _ = mzero
+
+checkelDiCheck :: String -> [String] -> [String] -> Bool
+checkelDiCheck var methodVars methodVarUsages =
+  notElem var methodVars && elem var methodVarUsages
