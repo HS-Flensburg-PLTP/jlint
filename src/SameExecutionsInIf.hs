592a2d27d9aec891906501505d016b7e0144f13e
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-12 08:48:50 +0200

    Multi-Statement Check if only if else
    If the if-then-else part contains no `if else ()` it is possible to check all lines from the beginning until one line is different

--- src/SameExecutionsInIf.hs
+++ src/SameExecutionsInIf.hs
@@ -16,8 +16,9 @@ checkFirstStatements (methodName, methodBody) path = do
   blockStmtms <- universeBi methodBody
   extractFirstStmt blockStmtms
   where
-    extractFirstStmt (BlockStmt (IfThenElse _ (StmtBlock (Block (x : _))) (StmtBlock (Block (y : _)))))
-      | x == y = [methodDiagnostic methodName "In an if-then-else statement, the first line of code is the same and can be swapped out." path]
+    extractFirstStmt (BlockStmt (IfThenElse _ (StmtBlock (Block (x : xs))) (StmtBlock (Block (y : ys)))))
+      | x == y =
+        [methodDiagnostic methodName ("In an if-then-else statement, " ++ show (checkNextStatements [xs, ys] path 1) ++ " line(s) of code is/are the same and can be swapped out.") path]
       | otherwise = []
     extractFirstStmt (BlockStmt (IfThenElse _ (StmtBlock (Block (x : _))) ifThenElse))
       | all (== x) (extractNestedIfThenElse ifThenElse) = [methodDiagnostic methodName "In an if-then-else statement, the first line of code is the same and can be swapped out." path]
@@ -26,3 +27,10 @@ checkFirstStatements (methodName, methodBody) path = do
     extractNestedIfThenElse (IfThenElse _ (StmtBlock (Block (x : _))) (StmtBlock (Block (y : _)))) = [x, y]
     extractNestedIfThenElse (IfThenElse _ (StmtBlock (Block (x : _))) ifThenElse) = x : extractNestedIfThenElse ifThenElse
     extractNestedIfThenElse _ = mzero
+
+checkNextStatements :: [[BlockStmt]] -> FilePath -> Int -> Int
+checkNextStatements [[], []] _ counter = counter
+checkNextStatements list path counter
+  | all (== head (map (\(x : _) -> x) list)) (concatMap (take 1) list) =
+    if all (\x -> length x >= 2) list then checkNextStatements (map tail list) path (counter + 1) else counter + 1
+  | otherwise = counter
