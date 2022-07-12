fe13781eee9b507cf9d38e79280af193c01e8d41
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-12 10:14:51 +0200

    restruct Code
    Every type of if-then-else can now be checked

--- src/SameExecutionsInIf.hs
+++ src/SameExecutionsInIf.hs
@@ -9,28 +9,26 @@ import RDF (Diagnostic (..), methodDiagnostic)
 check :: CompilationUnit -> FilePath -> [Diagnostic]
 check cUnit path = do
   methods <- extractMethods cUnit
-  checkFirstStatements methods path
+  checkMethodBlocks methods path
 
-checkFirstStatements :: (String, MethodBody) -> FilePath -> [Diagnostic]
-checkFirstStatements (methodName, methodBody) path = do
-  blockStmtms <- universeBi methodBody
-  extractFirstStmt blockStmtms
+checkMethodBlocks :: (String, MethodBody) -> FilePath -> [Diagnostic]
+checkMethodBlocks (methodName, methodBody) path = do
+  blockStmts <- universeBi methodBody
+  checkStmts (extractIfThenElseBlocks blockStmts)
   where
-    extractFirstStmt (BlockStmt (IfThenElse _ (StmtBlock (Block (x : xs))) (StmtBlock (Block (y : ys)))))
-      | x == y =
-        [methodDiagnostic methodName ("In an if-then-else statement, " ++ show (checkNextStatements [xs, ys] path 1) ++ " line(s) of code is/are the same and can be swapped out.") path]
-      | otherwise = []
-    extractFirstStmt (BlockStmt (IfThenElse _ (StmtBlock (Block (x : _))) ifThenElse))
-      | all (== x) (extractNestedIfThenElse ifThenElse) = [methodDiagnostic methodName "In an if-then-else statement, the first line of code is the same and can be swapped out." path]
-      | otherwise = []
-    extractFirstStmt _ = []
-    extractNestedIfThenElse (IfThenElse _ (StmtBlock (Block (x : _))) (StmtBlock (Block (y : _)))) = [x, y]
-    extractNestedIfThenElse (IfThenElse _ (StmtBlock (Block (x : _))) ifThenElse) = x : extractNestedIfThenElse ifThenElse
-    extractNestedIfThenElse _ = mzero
+    extractIfThenElseBlocks (BlockStmt stmt) = extractStmt stmt
+    extractIfThenElseBlocks _ = mzero
+    checkStmts [] = []
+    checkStmts list = createDiagnostic (checkAllStatements list 0)
+    createDiagnostic counter = if counter > 0 then [methodDiagnostic methodName ("In an if-then-else statement, " ++ show counter ++ " line(s) of code is/are the same and can be swapped out.") path] else []
 
-checkNextStatements :: [[BlockStmt]] -> FilePath -> Int -> Int
-checkNextStatements [[], []] _ counter = counter
-checkNextStatements list path counter
+checkAllStatements :: [[BlockStmt]] -> Int -> Int
+checkAllStatements list counter
   | all (== head (map (\(x : _) -> x) list)) (concatMap (take 1) list) =
-    if all (\x -> length x >= 2) list then checkNextStatements (map tail list) path (counter + 1) else counter + 1
+    if all (\statements -> length statements >= 2) list then checkAllStatements (map tail list) (counter + 1) else counter + 1
   | otherwise = counter
+
+extractStmt :: Stmt -> [[BlockStmt]]
+extractStmt (IfThenElse _ (StmtBlock (Block blockA)) (StmtBlock (Block blockB))) = [blockA, blockB]
+extractStmt (IfThenElse _ (StmtBlock (Block blockA)) ifThenElse) = blockA : extractStmt ifThenElse
+extractStmt _ = []
