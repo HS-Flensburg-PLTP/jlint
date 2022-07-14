2c3e0538af85e735aab184724dc28eb35304dd80
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-14 09:46:30 +0200

    Check now also from end of if-then-else block

--- src/SameExecutionsInIf.hs
+++ src/SameExecutionsInIf.hs
@@ -19,12 +19,15 @@ checkMethodBlocks (methodName, methodBody) path = do
     extractIfThenElseBlocks (BlockStmt stmt) = extractStmt stmt
     extractIfThenElseBlocks _ = mzero
     checkStmts [] = []
-    checkStmts list = createDiagnostic (checkAllStatements list 0)
-    createDiagnostic counter = if counter > 0 then [methodDiagnostic methodName ("In an if-then-else statement, " ++ show counter ++ " line(s) of code is/are the same and can be swapped out.") path] else []
+    checkStmts list = createDiagnostic (checkAllStatements list 0 + checkAllStatements (reverseList list) 0) (length (head list))
+    createDiagnostic counter listLength
+      | counter > 0 && counter < listLength = [methodDiagnostic methodName ("In an if-then-else statement, " ++ show counter ++ " line(s) of code is/are the same and can be swapped out.") path]
+      | counter >= listLength = [methodDiagnostic methodName "In an if-then-else statement, all lines of code are the same and can be swapped out." path]
+      | otherwise = []
 
 checkAllStatements :: [[BlockStmt]] -> Int -> Int
 checkAllStatements list counter
-  | all (== head (map (\(x : _) -> x) list)) (concatMap (take 1) list) =
+  | all (== head (map (\(firstStmtList : _) -> firstStmtList) list)) (concatMap (take 1) list) =
     if all (\statements -> length statements >= 2) list then checkAllStatements (map tail list) (counter + 1) else counter + 1
   | otherwise = counter
 
@@ -32,3 +35,6 @@ extractStmt :: Stmt -> [[BlockStmt]]
 extractStmt (IfThenElse _ (StmtBlock (Block blockA)) (StmtBlock (Block blockB))) = [blockA, blockB]
 extractStmt (IfThenElse _ (StmtBlock (Block blockA)) ifThenElse) = blockA : extractStmt ifThenElse
 extractStmt _ = []
+
+reverseList :: [[BlockStmt]] -> [[BlockStmt]]
+reverseList = map reverse
