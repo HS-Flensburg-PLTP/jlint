4389e7f5753cbbe9e0d52a56d5c81def5411fd80
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-14 13:13:26 +0200

    Rewrite Rule

--- src/CheckScope.hs
+++ src/CheckScope.hs
@@ -2,23 +2,9 @@ module CheckScope where
 
 import AST (extractMethods)
 import Control.Monad (MonadPlus (..))
-import Data.Function ((&))
 import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.Syntax
-import RDF (Diagnostic (..), simpleDiagnostic)
-
-check :: CompilationUnit -> FilePath -> [Diagnostic]
-check cUnit path = concatMap (\var -> if countMethodsWithCalls (extractMethods cUnit) var == 1 then [simpleDiagnostic ("Scope of variable " ++ var ++ " can be reduced.") path] else mzero) (extractClassVars cUnit)
-
-countMethodsWithCalls :: [(String, MethodBody)] -> String -> Int
-countMethodsWithCalls methods var =
-  methods
-    & filter (\(_, body) -> checkClassVarUsageInMethod var (extractMethodVariables body) (extractMethodVariableUsages body))
-    & length
-
-checkClassVarUsageInMethod :: String -> [String] -> [String] -> Bool
-checkClassVarUsageInMethod var methodVars methodVarUsages =
-  notElem var methodVars && elem var methodVarUsages
+import RDF (Diagnostic (..), methodDiagnostic)
 
 extractMethodVariables :: MethodBody -> [String]
 extractMethodVariables methodBody = do
@@ -28,17 +14,36 @@ extractMethodVariables methodBody = do
     extractNames (LocalVars _ _ varDecls) = map (\(VarDecl (VarId (Ident n)) _) -> n) varDecls
     extractNames _ = mzero
 
-extractMethodVariableUsages :: MethodBody -> [String]
-extractMethodVariableUsages methodBody = do
-  usedVariables <- universeBi methodBody
-  extractUsedVariables usedVariables
+extractMethodStatements :: MethodBody -> [Stmt]
+extractMethodStatements methodBody = do
+  statements <- universeBi methodBody
+  extractStatements statements
   where
-    extractUsedVariables (Name varList) = map (\(Ident n) -> n) varList
+    extractStatements (IfThen _ stmt) = [stmt]
+    extractStatements (IfThenElse _ stmt1 stmt2) = [stmt1, stmt2]
+    extractStatements (While _ stmt) = [stmt]
+    extractStatements (BasicFor _ _ _ stmt) = [stmt]
+    extractStatements (EnhancedFor _ _ _ _ stmt) = [stmt]
+    extractStatements (Do stmt _) = [stmt]
+    extractStatements _ = mzero
 
-extractClassVars :: CompilationUnit -> [String]
-extractClassVars cUnit = do
-  variables <- universeBi cUnit
-  extractVars variables
+check :: CompilationUnit -> FilePath -> [Diagnostic]
+check cUnit path = do
+  (methodName, methodBody) <- extractMethods cUnit
+  variables <- extractMethodVariables methodBody
+  if countVarCalls (extractMethodStatements methodBody) variables == 1
+    then [methodDiagnostic methodName ("The scope of variable " ++ variables ++ " can be reduced.") path]
+    else mzero
+
+countVarCalls :: [Stmt] -> String -> Int
+countVarCalls [] _ = 0
+countVarCalls (x : xs) var
+  | or (checkIfElement x var) = 1 + countVarCalls xs var
+  | otherwise = 0 + countVarCalls xs var
+
+checkIfElement :: Stmt -> String -> [Bool]
+checkIfElement stmt var = do
+  elements <- universeBi stmt
+  checkStatement elements var
   where
-    extractVars (FieldDecl _ _ varDecls) = map (\(VarDecl (VarId (Ident n)) _) -> n) varDecls
-    extractVars _ = mzero
+    checkStatement (Name varList) var = [Ident var `elem` varList]
