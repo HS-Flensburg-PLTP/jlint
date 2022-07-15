0dd4c9a9322740dcd4ff98bcfe1782cfd0b9052b
Lukas Nolde <lukas@medimad.de>
2022-07-15 13:59:07 +0200

    Get Main.hs to print paringerrors and reimplemented prettyprintoption

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@ module Main where
 import CheckNonFinalMethodAttributes
 import CheckNonPrivateAttributes
 -- import qualified Data.ByteString
+
+import Control.Monad (MonadPlus (..), unless)
 import Data.ByteString.Lazy.Internal
 import Data.Semigroup ((<>))
 import EmptyLoopBody (check)
@@ -18,8 +20,6 @@ import Options.Applicative
 import RDF
 import System.FilePath.Find
 import Text.Parsec.Error
-import Control.Monad (MonadPlus (..))
-
 
 main :: IO ()
 main = execParser opts >>= importJava
@@ -57,56 +57,55 @@ findAllJavaFiles :: FilePath -> IO [FilePath]
 findAllJavaFiles =
   find always (extension ==? ".java")
 
-
+-- can be done with map
 readAllFiles :: [FilePath] -> IO [(String, FilePath)]
 readAllFiles paths =
-  let
-    readAllFilesHelp :: [FilePath] -> [(String, FilePath)] -> IO [(String, FilePath)]
-    readAllFilesHelp pathList fileList =
-      case pathList of 
-        [] ->
-          return fileList
-        path : restPaths -> do
-          file <- readFile path
-          let fileres = (file, path)
-          readAllFilesHelp restPaths (return fileres)
-    in
-      readAllFilesHelp paths mzero
+  let readAllFilesHelp :: [FilePath] -> [(String, FilePath)] -> IO [(String, FilePath)]
+      readAllFilesHelp pathList fileList =
+        case pathList of
+          [] ->
+            return fileList
+          path : restPaths -> do
+            file <- readFile path
+            let fileres = (file, path)
+            readAllFilesHelp restPaths (return fileres)
+   in readAllFilesHelp paths mzero
 
-parseAllFiles :: [(String, FilePath)] -> ([(Text.Parsec.Error.ParseError, FilePath)],[(CompilationUnit, FilePath)])
+parseAllFiles :: [(String, FilePath)] -> ([(Text.Parsec.Error.ParseError, FilePath)], [(CompilationUnit, FilePath)])
 parseAllFiles files =
-  let 
-    parseAllfilesHelp :: [(String, FilePath)] -> ([(Text.Parsec.Error.ParseError, FilePath)],[(CompilationUnit, FilePath)]) -> ([(Text.Parsec.Error.ParseError, FilePath)],[(CompilationUnit, FilePath)])
-    parseAllfilesHelp fileList (errorList, cUnitList) =
-      case fileList of
-        [] ->
-          (errorList, cUnitList)
-        (file, path) : restFiles ->
-          case parser compilationUnit file of
-            Left error ->
-              parseAllfilesHelp restFiles ((error, path) : errorList, cUnitList)
-            Right cUnit ->
-              parseAllfilesHelp restFiles (errorList, (cUnit, path) : cUnitList)
-    in parseAllfilesHelp files (mzero, mzero)
+  let parseAllfilesHelp :: [(String, FilePath)] -> ([(Text.Parsec.Error.ParseError, FilePath)], [(CompilationUnit, FilePath)]) -> ([(Text.Parsec.Error.ParseError, FilePath)], [(CompilationUnit, FilePath)])
+      parseAllfilesHelp fileList (errorList, cUnitList) =
+        case fileList of
+          [] ->
+            (errorList, cUnitList)
+          (file, path) : restFiles ->
+            case parser compilationUnit file of
+              Left error ->
+                parseAllfilesHelp restFiles ((error, path) : errorList, cUnitList)
+              Right cUnit ->
+                parseAllfilesHelp restFiles (errorList, (cUnit, path) : cUnitList)
+   in parseAllfilesHelp files (mzero, mzero)
 
-
-
-
+--missing pretty option and does not print errors
 parseJava :: FilePath -> Bool -> IO ()
 parseJava rootDir pretty =
   do
     pathList <- findAllJavaFiles rootDir
     fileList <- readAllFiles pathList
-    let (errorResults, cUnitResults) = parseAllFiles fileList
-    let diagnostics = concatMap (\(cUnit, path) -> CheckNonFinalMethodAttributes.check cUnit path ++ CheckNonPrivateAttributes.check cUnit path ++ EmptyLoopBody.check cUnit path ++ NeedBraces.check cUnit path ++ NamingConventions.checkPackageName cUnit path) cUnitResults
-    putStrLn
-      ( Data.ByteString.Lazy.Internal.unpackChars
-          ( RDF.encodetojson
-              ( DiagnosticResult
-                  { diagnostics = diagnostics,
-                    resultSource = Just (Source {name = "jlint", sourceURL = Nothing}),
-                    resultSeverity = RDF.checkSeverityList (map RDF.severity diagnostics) -- emmits highest severity of all results in all files
-                  }
+    let (parsingErrors, cUnitResults) = parseAllFiles fileList
+    if pretty
+      then putStrLn (unlines (map (\(cUnit, _) -> prettyPrint cUnit) cUnitResults))
+      else do
+        let diagnostics = concatMap (\(cUnit, path) -> CheckNonFinalMethodAttributes.check cUnit path ++ CheckNonPrivateAttributes.check cUnit path ++ EmptyLoopBody.check cUnit path ++ NeedBraces.check cUnit path ++ NamingConventions.checkPackageName cUnit path) cUnitResults
+        putStrLn
+          ( Data.ByteString.Lazy.Internal.unpackChars
+              ( RDF.encodetojson
+                  ( DiagnosticResult
+                      { diagnostics = diagnostics,
+                        resultSource = Just (Source {name = "jlint", sourceURL = Nothing}),
+                        resultSeverity = RDF.checkSeverityList (map RDF.severity diagnostics) -- emmits highest severity of all results in all files
+                      }
+                  )
               )
           )
-      )
+        unless (null parsingErrors) $ print parsingErrors
