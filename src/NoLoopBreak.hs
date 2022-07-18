ffb7e64aaf4a7023d3eb6ff91d1b0c08f2d8609d
Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2022-07-18 08:22:56 +0200

    Refactor NoLoopBreak - extract local functions to TopLevel and add Reader Monad

--- src/NoLoopBreak.hs
+++ src/NoLoopBreak.hs
@@ -1,7 +1,7 @@
 module NoLoopBreak where
 
 import AST (extractMethods)
-import Control.Monad (MonadPlus (..))
+import Control.Monad.Reader
 import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.Syntax
 import RDF (Diagnostic (..), methodDiagnostic)
@@ -14,17 +14,25 @@ check cUnit path = do
 checkStatements :: (String, MethodBody) -> FilePath -> [Diagnostic]
 checkStatements (methodName, methodBody) path = do
   stmt <- universeBi methodBody
-  checkStatement stmt
-  where
-    checkStatement (While _ (StmtBlock (Block block))) = extractLoopBody block
-    checkStatement (Do (StmtBlock (Block block)) _) = extractLoopBody block
-    checkStatement (BasicFor _ _ _ (StmtBlock (Block block))) = extractLoopBody block
-    checkStatement (EnhancedFor _ _ _ _ (StmtBlock (Block block))) = extractLoopBody block
-    checkStatement _ = mzero
-    extractLoopBody ((BlockStmt block) : xs) = checkStmt block ++ extractLoopBody xs
-    extractLoopBody ((LocalClass _) : xs) = extractLoopBody xs
-    extractLoopBody ((LocalVars {}) : xs) = extractLoopBody xs
-    extractLoopBody [] = mzero
-    checkStmt (Return _) = return (methodDiagnostic methodName "Exit Loop with return." path)
-    checkStmt (Break _) = return (methodDiagnostic methodName "Exit Loop with break." path)
-    checkStmt _ = []
+  runReader (checkStatement stmt) (methodName, path)
+
+checkStatement :: Stmt -> Reader (String, FilePath) [Diagnostic]
+checkStatement (While _ (StmtBlock (Block block))) = extractLoopBody block
+checkStatement (Do (StmtBlock (Block block)) _) = extractLoopBody block
+checkStatement (BasicFor _ _ _ (StmtBlock (Block block))) = extractLoopBody block
+checkStatement (EnhancedFor _ _ _ _ (StmtBlock (Block block))) = extractLoopBody block
+checkStatement _ = return []
+
+extractLoopBody :: [BlockStmt] -> Reader (String, FilePath) [Diagnostic]
+extractLoopBody ((BlockStmt block) : xs) = do
+  (methodName, path) <- ask
+  elb <- extractLoopBody xs
+  return (checkStmt block methodName path ++ elb)
+extractLoopBody ((LocalClass _) : xs) = extractLoopBody xs
+extractLoopBody ((LocalVars {}) : xs) = extractLoopBody xs
+extractLoopBody [] = return []
+
+checkStmt :: Stmt -> String -> FilePath -> [Diagnostic]
+checkStmt (Return _) methodName path = [methodDiagnostic methodName "Exit Loop with return." path]
+checkStmt (Break _) methodName path = [methodDiagnostic methodName "Exit Loop with break." path]
+checkStmt _ _ _ = []
