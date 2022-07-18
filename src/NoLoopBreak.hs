faa39285201c06fc2b4cef5ad042f29d6ab86c38
Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2022-07-18 09:04:57 +0200

    Add Reader Monad to NoLoopBreak

--- src/NoLoopBreak.hs
+++ src/NoLoopBreak.hs
@@ -17,22 +17,35 @@ checkStatements (methodName, methodBody) path = do
   runReader (checkStatement stmt) (methodName, path)
 
 checkStatement :: Stmt -> Reader (String, FilePath) [Diagnostic]
-checkStatement (While _ (StmtBlock (Block block))) = extractLoopBody block
-checkStatement (Do (StmtBlock (Block block)) _) = extractLoopBody block
-checkStatement (BasicFor _ _ _ (StmtBlock (Block block))) = extractLoopBody block
-checkStatement (EnhancedFor _ _ _ _ (StmtBlock (Block block))) = extractLoopBody block
+checkStatement (While _ stmt) = checkStmt stmt
+checkStatement (Do stmt _) = checkStmt stmt
+checkStatement (BasicFor _ _ _ stmt) = checkStmt stmt
+checkStatement (EnhancedFor _ _ _ _ stmt) = checkStmt stmt
 checkStatement _ = return []
+
+checkNestedStatement :: Stmt -> Reader (String, FilePath) [Diagnostic]
+checkNestedStatement (StmtBlock (Block block)) = extractLoopBody block
+checkNestedStatement (IfThen _ stmt) = checkStmt stmt
+checkNestedStatement (IfThenElse _ stmt1 stmt2) = do
+  stm1 <- checkStmt stmt1
+  stm2 <- checkStmt stmt2
+  return (stm1 ++ stm2)
+checkNestedStatement stmt = checkStatement stmt
 
 extractLoopBody :: [BlockStmt] -> Reader (String, FilePath) [Diagnostic]
 extractLoopBody ((BlockStmt block) : xs) = do
-  (methodName, path) <- ask
+  stmtblock <- checkStmt block
   elb <- extractLoopBody xs
-  return (checkStmt block methodName path ++ elb)
+  return (stmtblock ++ elb)
 extractLoopBody ((LocalClass _) : xs) = extractLoopBody xs
 extractLoopBody ((LocalVars {}) : xs) = extractLoopBody xs
 extractLoopBody [] = return []
 
-checkStmt :: Stmt -> String -> FilePath -> [Diagnostic]
-checkStmt (Return _) methodName path = [methodDiagnostic methodName "Exit Loop with return." path]
-checkStmt (Break _) methodName path = [methodDiagnostic methodName "Exit Loop with break." path]
-checkStmt _ _ _ = []
+checkStmt :: Stmt -> Reader (String, FilePath) [Diagnostic]
+checkStmt (Return _) = do
+  (methodName, path) <- ask
+  return [methodDiagnostic methodName "Exit Loop with return." path]
+checkStmt (Break _) = do
+  (methodName, path) <- ask
+  return [methodDiagnostic methodName "Exit Loop with break." path]
+checkStmt stmt = checkNestedStatement stmt
