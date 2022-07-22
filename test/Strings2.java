eb5949739262d831bbfd65c44e958a82fb28e937
Lukas <63160505+luno5530@users.noreply.github.com>
2022-07-22 13:08:54 +0200

    Feature 54 default comes last regel (#56)
    * Use concate to add Ruels in Main.parseJava and implement DefaultcomesLast.hs
    
    * Remove unnessary helperfunction
    
    * Use concate to add Ruels in Main.parseJava and implement DefaultcomesLast.hs
    
    * Remove unnessary helperfunction
    
    * Import Dependencies to use HUnit
    
    * Use Ormulo to format DefaultcomesLast and remove redundant brackets
    
    * Use methodDiagnostic to create errormessage in defaultcomesLast.checkDefaultcomesLast - function
    
    * Remove shadowing in DefaultComesLast.checkDefaultComesLast
    
    * Remove legacy version of TestEmtpyLoopBody
    
    * Remove legacy version of TestEmtpyLoopBody
    
    * Apply ormulo to each and every file
    
    * Remove redundant brackets in main and run ormulo
    
    * fixing comments
    
    Co-authored-by: Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>

--- test/Strings2.java
+++ test/Strings2.java
@@ -8,6 +8,20 @@ public class Strings2 {
 
     public void testFunc(String name, final int age) {
         System.out.println("Hallo, hier wird geprintet");
+
+        switch(age) {
+            default:
+                System.out.println("This case should be defined last");
+            case 10:
+                System.out.println("This should be the first case");
+        }
+
+        switch(age) {
+            case 10:
+                System.out.println("This should be the first case");
+            default:
+                System.out.println("This case should be defined last");
+        }
     }
 }
 
---
+++ test/TestEmptyLoopBody.hs
@@ -0,0 +1,11 @@
+module TestEmptyLoopBody where
+
+import Language.Java.Parser (compilationUnit, parser)
+import Language.Java.Syntax
+
+parseJava :: String -> Maybe Language.Java.Syntax.CompilationUnit
+parseJava input = do
+  let result = parser compilationUnit input
+  case result of
+    Left _ -> Nothing
+    Right cUnit -> Just cUnit
