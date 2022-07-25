32d4fced973bfa040f4e4db90e3d79e4a5816745
Lukas Nolde <lukas@medimad.de>
2022-07-25 13:46:24 +0200

    Add EmptyLoopBody with capital case

---
+++ test/EmptyLoopBody/EmptyWhileLoop.java
@@ -0,0 +1,10 @@
+package de.fuas.algorithms;
+
+public class Strings2 {
+
+    public void testFunc(String name, final int age) {
+        int i = 50;
+        int j = 150;
+        while(++i < --j);
+    }
+}
