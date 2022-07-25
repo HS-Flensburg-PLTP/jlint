dd40d9faba6e5fe8bd7b37b26c1202ff5636ef5a
Lukas Nolde <lukas@medimad.de>
2022-07-25 13:58:06 +0200

    Remove expted - constant from EmptyLoopBody.Tests.hs

--- test/EmptyLoopBody/Tests.hs
+++ test/EmptyLoopBody/Tests.hs
@@ -62,37 +62,3 @@ testEmptEnhancedForLoop =
               checkMessage diagResult "Method testFunc: A ForEach-Lopp has a empty loop body."
           )
       ]
-
-{-testEmptyWhileLoop =
-  test -- test can be called on List of testables
-    [ "Empty While Loop"
-        ~: withCUnit -- ~: adds label and applies assertion to Testcase (Testcase . assert) -> this is a testcase
-          "/test/EmptyLoopBody.java"
-          ( \(path, cUnit) -> do
-              let actual = EmptyLoopBody.check cUnit path
-              actual @=? [expected] -- asserition, can do multiple in one Test, which would only be exuted if the forme succeded
-          )
-    ]
--}
-expected :: Diagnostic
-expected =
-  Diagnostic
-    { message = "Method testFunc: A While-Loop has a empty loop body.",
-      location =
-        Location
-          { RDF.path = "/Users/lukasnolde/Documents/Informatik/Semester06/Projekt/Repositories/jlint/test/emptyLoopBody/EmptyLoopBody.java",
-            locationRange = Nothing
-          },
-      severity = Warning,
-      source = Nothing,
-      code = Nothing,
-      suggestions = Nothing,
-      originalOutput = Nothing
-    }
-
-{- TODO:
-    1. Find Structure for test: e.g: Folder for each test-, input-, expected- file
-    2. Suggest: using balckboxtest, define classes of equivalence ()
-        ,define Unit test as well as Integrationtests(to check interaction between parser and jlint)
-    3. Convert RDF.path to work on every device
--}
