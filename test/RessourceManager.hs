63decce8ff83f40f6d60b825d4eda4a6b25b5d55
Lukas Nolde <lukas@medimad.de>
2022-07-27 19:57:26 +0200

    Rename functions in ressourcemanager

--- test/RessourceManager.hs
+++ test/RessourceManager.hs
@@ -91,8 +91,8 @@ fileLexer input rootDir =
     mergeNameCode fName sCode =
       (sCode, fName)
 
-filterParsingResults :: [(Either ParseError CompilationUnit, FilePath)] -> [IO (CompilationUnit, FilePath)]
-filterParsingResults =
+filterJavaParsingResults :: [(Either ParseError CompilationUnit, FilePath)] -> [IO (CompilationUnit, FilePath)]
+filterJavaParsingResults =
   map
     ( \(paringResult, corresPath) -> case paringResult of
         Left error ->
@@ -117,7 +117,7 @@ parseJava path = do
           )
           formatedInput
 
-  return (filterParsingResults result)
+  return (filterJavaParsingResults result)
 
 withCUnit :: FilePath -> ([IO (CompilationUnit, FilePath)] -> IO [IO ()]) -> IO [IO ()]
 withCUnit relativePath =
