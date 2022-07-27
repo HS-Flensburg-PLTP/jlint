eec6e32a19a4487a119b7b1c02856899c24ef69a
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-27 08:12:48 +0200

    Feature 103 namingconventions testfaelle (#104)
    * Tests for Package, Methods, Parameter, StaticVar
    
    * Tests for LocalFinalVars
    
    * Test for LocalVariables
    
    * Add MemberName tests
    
    * Add TypeName Tests
    
    * format Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,9 @@
 import EmptyLoopBody.Tests (testAllEmptyLoopBodies)
+import NamingConventions.Tests
 import NoLoopBreak.Tests (testAllNoLoopBreaks)
 
 main :: IO ()
 main = do
   testAllNoLoopBreaks
   testAllEmptyLoopBodies
+  NamingConventions.Tests.testAll
