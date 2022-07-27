61c05dc97b39ea9d4411bbc6d7cb74b072bcb23f
Lukas Nolde <lukas@medimad.de>
2022-07-27 18:10:15 +0200

    Add new EmptyLoopBodytest.hs

--- test/EmptyLoopBody/EmptyBasicForLoop.java
+++ test/EmptyLoopBody/EmptyBasicForLoop.java
@@ -1,3 +1,19 @@
+-------------------------
+EmptyBody
++++++++++++++++++++++++++
+package de.fuas.algorithms;
+
+public class Strings2 {
+
+    public void testFunc(String name, final int age) {
+        int i = 50;
+        int j = 150;
+        for(int i = 0; i <= 3; i ++){};
+    }
+}
+-------------------------
+WithoutBody
++++++++++++++++++++++++++
 package de.fuas.algorithms;
 
 public class Strings2 {
@@ -6,6 +22,5 @@ public class Strings2 {
         int i = 50;
         int j = 150;
         for(int i = 0; i <= 3; i ++);
-        // for(int i = 0; i <= 3 i ++){} TODO: IS NOT SEEN AS EMPTY FOR LOOP    
     }
 }
