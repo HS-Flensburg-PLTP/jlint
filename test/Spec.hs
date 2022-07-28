5f360388b51ec8fd123c8665b311c8370de35996
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-28 13:14:44 +0200

    add Tests (#127)

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,10 +2,12 @@ import EmptyLoopBody.Tests (testAllEmptyLoopBodies)
 import NamingConventions.Tests
 import NeedBraces.Tests
 import NoLoopBreak.Tests (testAllNoLoopBreaks)
+import SimplifyBooleanReturn.Tests
 
 main :: IO ()
 main = do
   testAllEmptyLoopBodies
   testAllNoLoopBreaks
   NamingConventions.Tests.testAll
+  SimplifyBooleanReturn.Tests.testAll
   NeedBraces.Tests.testAll
