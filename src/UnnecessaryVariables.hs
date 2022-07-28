04fb8304c4a59128811cf9bc3c9ae1e32f8e3ab0
Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2022-07-28 08:04:05 +0200

    Bugfixing

--- src/UnnecessaryVariables.hs
+++ src/UnnecessaryVariables.hs
@@ -36,7 +36,7 @@ extractReturnedVars methodBody = do
 
 checkNecessaryVars :: [String] -> [String] -> [String] -> String -> FilePath -> [Diagnostic]
 checkNecessaryVars declaredVars usedVars returnedVars methodName path =
-  if checkLengthofFilterUsedVars declaredVars usedVars >= 1 && checkLenghtofFilterReturnedVars declaredVars returnedVars <= 1
+  if checkLengthofFilterUsedVars declaredVars usedVars == 1 && checkLenghtofFilterReturnedVars declaredVars returnedVars <= 1
     then map (\var -> methodDiagnostic methodName ("Variable " ++ var ++ " is unnecessary") path) declaredVars
     else []
 
