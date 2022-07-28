595d3548f3cbc9539f14a73f3d1dd6e2700e95ab
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-28 10:42:20 +0200

    add Tests

---
+++ test/SimplifyBooleanReturn/Tests.hs
@@ -0,0 +1,52 @@
+module SimplifyBooleanReturn.Tests where
+
+import CheckResults
+import Language.Java.Parser (compilationUnit, parser)
+import Language.Java.Syntax
+import RDF
+import RessourceManager
+import SimplifyBooleanReturn (check)
+import Test.HUnit
+
+testAll :: IO ()
+testAll = do
+  testIfThen <- testIfThenIO
+  testIfThenElse <- testIfThenElseIO
+  runTestTT ("SimplifyBooleanReturn" ~: [testIfThen, testIfThenElse])
+  return ()
+
+testIfThenIO :: IO Test
+testIfThenIO =
+  do
+    assertionList <-
+      withCUnit
+        "/test/SimplifyBooleanReturn/IfThen.java"
+        ( return
+            . map
+              ( \inputCode -> do
+                  (cUnit, path) <- inputCode
+                  let diagResults = check cUnit path
+                  checkMessage diagResults "Method testFunc: A if-then part with literal return can be simplified." path
+                  checkPath diagResults path
+              )
+        )
+
+    return ("IfThen" ~: test (map TestCase assertionList))
+
+testIfThenElseIO :: IO Test
+testIfThenElseIO =
+  do
+    assertionList <-
+      withCUnit
+        "/test/SimplifyBooleanReturn/IfThenElse.java"
+        ( return
+            . map
+              ( \inputCode -> do
+                  (cUnit, path) <- inputCode
+                  let diagResults = check cUnit path
+                  checkMessage diagResults "Method testFunc: A if-then-else part with literal return can be simplified." path
+                  checkPath diagResults path
+              )
+        )
+
+    return ("IfThen" ~: test (map TestCase assertionList))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,11 @@
 import EmptyLoopBody.Tests (testAllEmptyLoopBodies)
 import NamingConventions.Tests
 import NoLoopBreak.Tests (testAllNoLoopBreaks)
+import SimplifyBooleanReturn.Tests
 
 main :: IO ()
 main = do
   testAllEmptyLoopBodies
   testAllNoLoopBreaks
   NamingConventions.Tests.testAll
+  SimplifyBooleanReturn.Tests.testAll
