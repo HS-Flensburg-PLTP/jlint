6946b1c92b548ef7fe4eee95b16e762bb0480ca7
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-28 09:09:17 +0200

    second java-case for enhancedFor

--- test/NeedBraces/NoEnhancedForBraces.java
+++ test/NeedBraces/NoEnhancedForBraces.java
@@ -7,3 +7,11 @@ class Test {
             System.out.println(i);
     }
 }
+-------------------------
+NoBracesEnhancedForSingleLine
++++++++++++++++++++++++++
+class Test {
+    public void testFunc() {
+        for (int i : arr) System.out.println(i);
+    }
+}
