f439eab90396647543b6805336f9677146fd3299
Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2022-07-28 08:19:55 +0200

    Refactor TestUnnecessaryVariables

--- test/UnnecessaryVariables/Tests.hs
+++ test/UnnecessaryVariables/Tests.hs
@@ -1,6 +1,8 @@
 module UnnecessaryVariables.Tests (testAllUnnecessaryVariables) where
 
 import CheckResults
+import Language.Java.Parser (compilationUnit, parser)
+import Language.Java.Syntax
 import RDF
 import RessourceManager
 import Test.HUnit
@@ -8,17 +10,27 @@ import UnnecessaryVariables (checkMethodVars)
 
 testAllUnnecessaryVariables :: IO ()
 testAllUnnecessaryVariables = do
-  runTestTT (TestList [testUnnecessaryVariables])
+  testUnnecessaryVariables <- testUnnecessaryVariablesIO
+  runTestTT
+    ( "Unnecessary Variables"
+        ~: [testUnnecessaryVariables]
+    )
   return ()
 
-testUnnecessaryVariables =
-  "Unnecessary Variables"
-    ~: test -- test can be called on List of testables
-      [ withCUnit
-          "/test/UnnecessaryVariables/UnnecessaryVariables.java"
-          ( \(path, cUnit) -> do
-              let diagResult = checkMethodVars cUnit path
-              checkPath diagResult path
-              checkMessage diagResult "Method testFunc: Variable a is unnecessary"
-          )
-      ]
+testUnnecessaryVariablesIO :: IO Test
+testUnnecessaryVariablesIO =
+  do
+    assertionList <-
+      withCUnit
+        "/test/UnnecessaryVariables/UnnecessaryVariables.java"
+        ( return
+            . map
+              ( \inputCode -> do
+                  (cUnit, path) <- inputCode
+                  let diagResults = UnnecessaryVariables.checkMethodVars cUnit path
+                  checkMessage diagResults "Method testFunc: Variable a is unnecessary" path
+                  checkPath diagResults path
+              )
+        )
+
+    return ("UnnecessaryVariables" ~: test (map TestCase assertionList))
