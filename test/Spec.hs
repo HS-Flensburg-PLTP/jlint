f317aed25dce637db65c40142cbce84e25ee7414
Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>
2022-07-28 11:20:16 +0200

    implement Test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,11 @@
 import EmptyLoopBody.Tests (testAllEmptyLoopBodies)
 import NamingConventions.Tests
 import NoLoopBreak.Tests (testAllNoLoopBreaks)
+import SameExecutionsInIf.Tests
 
 main :: IO ()
 main = do
   testAllEmptyLoopBodies
   testAllNoLoopBreaks
   NamingConventions.Tests.testAll
+  SameExecutionsInIf.Tests.testAll
