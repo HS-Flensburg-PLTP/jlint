2f98a47780b81714372adc8ea1bd47029a1d0dce
Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2022-07-28 08:55:17 +0200

    Implement test for CheckNonFinalMethodAttributes

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,11 @@
 import EmptyLoopBody.Tests (testAllEmptyLoopBodies)
 import NamingConventions.Tests
 import NoLoopBreak.Tests (testAllNoLoopBreaks)
+import NonFinalMethodAttributes.Tests (testAllNonFinalMethodAttributes)
 
 main :: IO ()
 main = do
   testAllEmptyLoopBodies
   testAllNoLoopBreaks
   NamingConventions.Tests.testAll
+  testAllNonFinalMethodAttributes
