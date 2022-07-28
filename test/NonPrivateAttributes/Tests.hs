bea9abae2edae30e9d944c658d990877eff59839
Philipp Theis <62700798+Phillemove@users.noreply.github.com>
2022-07-28 13:33:43 +0200

    Implement test for CheckNonPrivateAttributes (#119)
    Co-authored-by: Hauke Marquard <62841369+AdmiralMurtho@users.noreply.github.com>

---
+++ test/NonPrivateAttributes/Tests.hs
@@ -0,0 +1,37 @@
+module NonPrivateAttributes.Tests (testAllNonPrivateAttributes) where
+
+import CheckNonPrivateAttributes (check)
+import CheckResults
+import Language.Java.Parser (compilationUnit, parser)
+import Language.Java.Syntax
+import RDF
+import RessourceManager
+import Test.HUnit
+
+testAllNonPrivateAttributes :: IO ()
+testAllNonPrivateAttributes = do
+  testNonPrivateAttributes <- testNonPrivateAttributesIO
+
+  runTestTT
+    ( "Non Private Attributes"
+        ~: [testNonPrivateAttributes]
+    )
+  return ()
+
+testNonPrivateAttributesIO :: IO Test
+testNonPrivateAttributesIO =
+  do
+    assertionList <-
+      withCUnit
+        "/test/NonPrivateAttributes/NonPrivateAttributes.java"
+        ( return
+            . map
+              ( \inputCode -> do
+                  (cUnit, path) <- inputCode
+                  let diagResults = CheckNonPrivateAttributes.check cUnit path
+                  checkMessage diagResults "Method myname: Is not declared as private" path
+                  checkPath diagResults path
+              )
+        )
+
+    return ("Non Private Attributes" ~: test (map TestCase assertionList))
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,14 +3,17 @@ import EmptyLoopBody.Tests (testAllEmptyLoopBodies)
 import NamingConventions.Tests
 import NeedBraces.Tests
 import NoLoopBreak.Tests (testAllNoLoopBreaks)
+import NonPrivateAttributes.Tests (testAllNonPrivateAttributes)
 import SameExecutionsInIf.Tests
 import SimplifyBooleanReturn.Tests
+
 
 main :: IO ()
 main = do
   testAllEmptyLoopBodies
   testAllNoLoopBreaks
   NamingConventions.Tests.testAll
+  testAllNonPrivateAttributes
   SameExecutionsInIf.Tests.testAll
   DefaultComesLast.Tests.testAll
   SimplifyBooleanReturn.Tests.testAll
