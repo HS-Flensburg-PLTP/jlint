e8180252fc2f2191b552e5587f0b8b12c43e2f0d
Philipp Theis <jan-philipp.theis@stud.hs-flensburg.de>
2022-07-28 08:42:24 +0200

    Implement test for CheckNonPrivateAttributes

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,11 @@
 import EmptyLoopBody.Tests (testAllEmptyLoopBodies)
 import NamingConventions.Tests
 import NoLoopBreak.Tests (testAllNoLoopBreaks)
+import NonPrivateAttributes.Tests (testAllNonPrivateAttributes)
 
 main :: IO ()
 main = do
   testAllEmptyLoopBodies
   testAllNoLoopBreaks
   NamingConventions.Tests.testAll
+  testAllNonPrivateAttributes
