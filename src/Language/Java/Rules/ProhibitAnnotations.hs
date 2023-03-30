a367f861fc56de7ffd3a1ca82fc6508ebb0a49c1
Christian Oehlers <averodas@mailbox.org>
2023-03-30 13:32:19 +0200

    Add tests for feature223

---
+++ src/Language/Java/Rules/ProhibitAnnotations.hs
@@ -0,0 +1,18 @@
+module Language.Java.Rules.ProhibitAnnotations where
+
+{-
+    NormalAnnotation
+
+    annName :: Name
+    annKV :: [(Ident, ElementValue)]
+
+SingleElementAnnotation
+
+    annName :: Name
+    annValue :: ElementValue
+
+MarkerAnnotation
+
+    annName :: Name
+
+    -}
