4c90de8473bc4028b6ec759428ae037fe2baec00
Christian Oehlers <averodas@mailbox.org>
2023-04-02 14:08:59 +0200

    Add Test Infrastructure for feature 223

--- test/ProhibitAnnotationsTests.hs
+++ test/ProhibitAnnotationsTests.hs
@@ -1,1 +1,23 @@
 module ProhibitAnnotationsTests where
+
+import Control.Monad.Identity (zipWithM_)
+import Language.Java.Rules.ProhibitAnnotations as ProhibitAnnotations (check)
+import Language.Java.Syntax (CompilationUnit)
+import RDF
+import Test.HUnit
+import qualified Tests
+
+tests :: Test
+tests =
+  let file = "/test/java/ProhibitAnnotations.java"
+   in test [file ~: Tests.withParsedJavaFile file prohibitAnnotations]
+
+prohibitAnnotations :: CompilationUnit -> FilePath -> Assertion
+prohibitAnnotations cUnit path = do
+  let expectedRanges = []
+  let diagnostics = ProhibitAnnotations.check cUnit path
+  assertEqual "Check Number of messages" (length expectedRanges) (length diagnostics)
+  zipWithM_
+    (assertEqual "Check range")
+    (map Just expectedRanges)
+    (map (range . location) diagnostics)
