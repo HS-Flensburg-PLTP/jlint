2b42f57c2c007df1e7bb2699a38bb63ed3d52daf
Philip Resech <pre@lambda9.de>
2023-04-04 14:14:10 +0200

    Update format

--- src/Language/Java/Rules/AvoidMultipleTopLevelDecl.hs
+++ src/Language/Java/Rules/AvoidMultipleTopLevelDecl.hs
@@ -1,22 +1,22 @@
 module Language.Java.Rules.AvoidMultipleTopLevelDecl (check) where
-import Control.Monad (MonadPlus(mzero))
+
+import Control.Monad (MonadPlus (mzero))
+import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.Syntax
 import qualified RDF
-import Data.Generics.Uniplate.Data (universeBi)
 
 check :: CompilationUnit -> FilePath -> [RDF.Diagnostic]
 check cUnit path = do
   comUnit <- universeBi cUnit
   checkComUnit comUnit
   where
-    checkComUnit (CompilationUnit _ _ typeDecls ) =
-        if length typeDecls > 1
-            then
-                [
-                    RDF.rangeDiagnostic
-                    "Language.Java.Rules.AvoidMultipleTopLevelDecl"
-                    "Jede Datei sollte immer nur eine Top-Level-Klasse beinhalten."
-                    dummySourceSpan
-                    path
-                ]
-            else mzero
+    checkComUnit (CompilationUnit _ _ typeDecls) =
+      if length typeDecls > 1
+        then
+          [ RDF.rangeDiagnostic
+              "Language.Java.Rules.AvoidMultipleTopLevelDecl"
+              "Jede Datei sollte immer nur eine Top-Level-Klasse beinhalten."
+              dummySourceSpan
+              path
+          ]
+        else mzero
