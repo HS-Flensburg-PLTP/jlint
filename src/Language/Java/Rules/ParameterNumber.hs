0c5f82e300e636c0823be8a11ca0945dbe59b919
Tom <tombecke77@gmail.com>
2023-04-11 15:51:59 +0200

    Add constant for max parameter number

--- src/Language/Java/Rules/ParameterNumber.hs
+++ src/Language/Java/Rules/ParameterNumber.hs
@@ -1,6 +1,6 @@
 module Language.Java.Rules.ParameterNumber where
 
-import Control.Monad
+import Control.Monad (MonadPlus (mzero))
 import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.Syntax
 import qualified RDF
@@ -8,13 +8,16 @@ import qualified RDF
 check :: CompilationUnit -> FilePath -> [RDF.Diagnostic]
 check cUnit path = do
   MethodDecl span _ _ _ _ paramList _ _ _ <- universeBi cUnit
-  if length paramList > 5
+  if length paramList > maxNumber
     then
       return
         ( RDF.rangeDiagnostic
             "Language.Java.Rules.ParameterNumber"
-            "Es sollten nicht mehr als 5 Parameter für eine Funktion verwendet werden."
+            ("Es sollten nicht mehr als" ++ show maxNumber ++ "Parameter für eine Funktion verwendet werden.")
             span
             path
         )
     else mzero
+
+maxNumber :: Int
+maxNumber = 5
