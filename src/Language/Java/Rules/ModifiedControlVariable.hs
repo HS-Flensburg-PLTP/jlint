10176da21f9927cf8da6abcef319b782f0200cb4
Joscha Konstantin Groth <joscha.groth@stud.hs-flensburg.de>
2023-04-11 16:09:31 +0200

    Use ident

--- src/Language/Java/Rules/ModifiedControlVariable.hs
+++ src/Language/Java/Rules/ModifiedControlVariable.hs
@@ -5,6 +5,7 @@ import Data.Generics.Uniplate.Data
 import qualified Data.List
 import Language.Java.Pretty
 import Language.Java.Syntax
+import Language.Java.Syntax.VarDecl (ident)
 import qualified RDF
 
 check :: CompilationUnit -> FilePath -> [RDF.Diagnostic]
@@ -13,7 +14,7 @@ check cUnit path = do
   checkBasicFor stmt
   where
     checkBasicFor (BasicFor (Just (ForLocalVars _ _ forVarDecls)) _ _ forStmt) = do
-      varDeclIdent <- map (\(VarDecl (VarId varDeclIdentStr) _) -> varDeclIdentStr) forVarDecls
+      varDeclIdent <- map ident forVarDecls
       expr <- universeBi forStmt
       checkBasicForModifiedControlVariable varDeclIdent expr
       where
