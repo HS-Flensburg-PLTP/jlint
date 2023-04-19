3350c2f1efe817a90cc3f6079aeee896901c9829
Philip Resech <philip.resech@stud.hs-flensburg.de>
2023-04-19 14:13:58 +0200

    Apply requested changes

--- src/Language/Java/Rules/AvoidMultipleTopLevelDecl.hs
+++ src/Language/Java/Rules/AvoidMultipleTopLevelDecl.hs
@@ -5,11 +5,8 @@ import Language.Java.Syntax
 import qualified RDF
 
 check :: CompilationUnit -> FilePath -> [RDF.Diagnostic]
-check cUnit path = do
-  checkComUnit cUnit
-  where
-    checkComUnit (CompilationUnit _ _ (_ : typeDecl : _)) = message (typeDeclSourceSpan typeDecl) path
-    checkComUnit _ = mzero
+check (CompilationUnit _ _ (_ : typeDecl : _)) path = [message (typeDeclSourceSpan typeDecl) path]
+check _ _ = mzero
 
 typeDeclSourceSpan :: TypeDecl -> SourceSpan
 typeDeclSourceSpan (ClassTypeDecl (ClassDecl span _ _ _ _ _ _)) = span
@@ -17,11 +14,8 @@ typeDeclSourceSpan (ClassTypeDecl (EnumDecl span _ _ _ _)) = span
 typeDeclSourceSpan (InterfaceTypeDecl (InterfaceDecl span _ _ _ _ _ _ _)) = span
 typeDeclSourceSpan (ClassTypeDecl (RecordDecl span _ _ _ _ _ _)) = span
 
-message :: SourceSpan -> FilePath -> [RDF.Diagnostic]
-message span path =
-  [ RDF.rangeDiagnostic
-      "Language.Java.Rules.AvoidMultipleTopLevelDecl"
-      "Jede Datei sollte immer nur eine Top-Level-Deklaration beinhalten."
-      span
-      path
-  ]
+message :: SourceSpan -> FilePath -> RDF.Diagnostic
+message =
+  RDF.rangeDiagnostic
+    "Language.Java.Rules.AvoidMultipleTopLevelDecl"
+    "Jede Datei sollte immer nur eine Top-Level-Deklaration beinhalten."
