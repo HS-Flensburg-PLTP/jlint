4bfb1f7a99057439e0909b8b2832c18c734c20ce
Christian Oehlers <averodas@mailbox.org>
2023-04-20 13:30:27 +0200

    Add Show to Rank-Type, add checkForPublic function, refactor checkRank

--- src/Language/Java/Rules/DeclarationOrder.hs
+++ src/Language/Java/Rules/DeclarationOrder.hs
@@ -2,6 +2,7 @@ module Language.Java.Rules.DeclarationOrder (check) where
 
 import Control.Monad (MonadPlus (mzero))
 import Data.Generics.Uniplate.Data (universeBi)
+import Data.Maybe (maybeToList)
 import Language.Java.Syntax
 import qualified RDF
 
@@ -16,7 +17,19 @@ data Rank
   | InstancePrivateField
   | Constructor
   | Method
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
+
+instance Show Rank where
+  show StaticPublicField = "static public Variable"
+  show StaticProtectedField = "static protected Variable"
+  show StaticPackageField = "static package Variable"
+  show StaticPrivateField = "static private Variable"
+  show InstancePublicField = "public Variable"
+  show InstanceProtectedField = "protected Variable"
+  show InstancePackageField = "package Variable"
+  show InstancePrivateField = "private Variable"
+  show Constructor = "Konstruktor"
+  show Method = "Methode"
 
 check :: CompilationUnit -> FilePath -> [RDF.Diagnostic]
 check cUnit path = checkRank path (extractMemberDecls cUnit)
@@ -24,41 +37,44 @@ check cUnit path = checkRank path (extractMemberDecls cUnit)
 extractMemberDecls :: CompilationUnit -> [(Rank, SourceSpan)]
 extractMemberDecls cUnit = do
   toplvlDecl <- universeBi cUnit
-  checkTopLvlStmts toplvlDecl
+  maybeToList (checkTopLvlStmts toplvlDecl)
 
-checkTopLvlStmts :: MemberDecl -> [(Rank, SourceSpan)]
+checkTopLvlStmts :: MemberDecl -> Maybe (Rank, SourceSpan)
 checkTopLvlStmts toplvlDecl = case toplvlDecl of
   FieldDecl sourceSpan mods _ _ ->
     if Static `elem` mods
       then
-        if Public `elem` mods
-          then [(StaticPublicField, sourceSpan)]
+        if checkForPublic mods
+          then Just (StaticPublicField, sourceSpan)
           else
             if Protected `elem` mods
-              then [(StaticProtectedField, sourceSpan)]
+              then Just (StaticProtectedField, sourceSpan)
               else
                 if Private `elem` mods
-                  then [(StaticPrivateField, sourceSpan)]
-                  else [(StaticPackageField, sourceSpan)]
+                  then Just (StaticPrivateField, sourceSpan)
+                  else Just (StaticPackageField, sourceSpan)
       else
-        if Public `elem` mods
-          then [(InstancePublicField, sourceSpan)]
+        if checkForPublic mods
+          then Just (InstancePublicField, sourceSpan)
           else
             if Protected `elem` mods
-              then [(InstanceProtectedField, sourceSpan)]
+              then Just (InstanceProtectedField, sourceSpan)
               else
                 if Private `elem` mods
-                  then [(InstancePrivateField, sourceSpan)]
-                  else [(InstancePackageField, sourceSpan)]
-  ConstructorDecl sourceSpan _ _ _ _ _ _ -> [(Constructor, sourceSpan)]
-  MethodDecl sourceSpan _ _ _ _ _ _ _ _ -> [(Method, sourceSpan)]
+                  then Just (InstancePrivateField, sourceSpan)
+                  else Just (InstancePackageField, sourceSpan)
+  ConstructorDecl sourceSpan _ _ _ _ _ _ -> Just (Constructor, sourceSpan)
+  MethodDecl sourceSpan _ _ _ _ _ _ _ _ -> Just (Method, sourceSpan)
   _ -> mzero
+
+checkForPublic :: [Modifier] -> Bool
+checkForPublic [] = False
+checkForPublic ((Public _) : _) = True
+checkForPublic (_ : xs) = checkForPublic xs
 
 checkRank :: FilePath -> [(Rank, SourceSpan)] -> [RDF.Diagnostic]
 checkRank path declOrder =
-  map
-    (\((rank, sourcespan), _) -> RDF.rangeDiagnostic "Language.Java.Rules.DeclarationOrder" (show rank ++ " an der falschen Stelle deklariert.") sourcespan path)
-    ( filter
-        (\((rankA, _), (rankB, _)) -> rankA > rankB)
-        (zip declOrder (tail declOrder))
-    )
+  map (createError . fst) (filter (\((rankA, _), (rankB, _)) -> rankA > rankB) (zip declOrder (tail declOrder)))
+  where
+    createError :: (Rank, SourceSpan) -> RDF.Diagnostic
+    createError (rank, sourcespan) = RDF.rangeDiagnostic "Language.Java.Rules.DeclarationOrder" (show rank ++ " an der falschen Stelle deklariert.") sourcespan path
