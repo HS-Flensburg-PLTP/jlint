822099dd419395be51cbcb1144b0cb78d196f3cd
Joscha Konstantin Groth <joscha.groth@stud.hs-flensburg.de>
2023-04-20 14:12:51 +0200

    Use new tests and ranges

--- test/ModifiedControlVariableTests.hs
+++ test/ModifiedControlVariableTests.hs
@@ -2,6 +2,7 @@ module ModifiedControlVariableTests where
 
 import Control.Monad (zipWithM_)
 import Language.Java.Rules.ModifiedControlVariable (check)
+import qualified Language.Java.Rules.ModifiedControlVariable as ModifiedControlVariable
 import Language.Java.Syntax
 import qualified RDF
 import Test.HUnit
@@ -9,64 +10,23 @@ import qualified Tests
 
 tests :: Test
 tests =
-  let file = "/test/java/ModifiedControlVariable.java"
-   in test [file ~: Tests.withParsedJavaFile file modifiedControlVariable]
+  Tests.rangesTest
+    expectedRanges
+    "ModifiedControlVariable.java"
+    ModifiedControlVariable.check
 
-modifiedControlVariable :: CompilationUnit -> FilePath -> Assertion
-modifiedControlVariable cUnit path = do
-  let expectedRanges =
-        [ RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 10, RDF.column = Just 13},
-              RDF.end = Just (RDF.Position {RDF.line = 10, RDF.column = Just 19})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 18, RDF.column = Just 13},
-              RDF.end = Just (RDF.Position {RDF.line = 18, RDF.column = Just 22})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 22, RDF.column = Just 18},
-              RDF.end = Just (RDF.Position {RDF.line = 22, RDF.column = Just 20})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 29, RDF.column = Just 14},
-              RDF.end = Just (RDF.Position {RDF.line = 29, RDF.column = Just 16})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 31, RDF.column = Just 17},
-              RDF.end = Just (RDF.Position {RDF.line = 31, RDF.column = Just 19})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 38, RDF.column = Just 14},
-              RDF.end = Just (RDF.Position {RDF.line = 38, RDF.column = Just 16})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 39, RDF.column = Just 43},
-              RDF.end = Just (RDF.Position {RDF.line = 39, RDF.column = Just 45})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 40, RDF.column = Just 17},
-              RDF.end = Just (RDF.Position {RDF.line = 40, RDF.column = Just 19})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 41, RDF.column = Just 46},
-              RDF.end = Just (RDF.Position {RDF.line = 41, RDF.column = Just 48})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 42, RDF.column = Just 21},
-              RDF.end = Just (RDF.Position {RDF.line = 42, RDF.column = Just 23})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 51, RDF.column = Just 18},
-              RDF.end = Just (RDF.Position {RDF.line = 51, RDF.column = Just 20})
-            },
-          RDF.Range
-            { RDF.start = RDF.Position {RDF.line = 59, RDF.column = Just 18},
-              RDF.end = Just (RDF.Position {RDF.line = 59, RDF.column = Just 20})
-            }
-        ]
-  let diagnostic = check cUnit path
-  assertEqual "Check number of messages" (length expectedRanges) (length diagnostic)
-  zipWithM_
-    (assertEqual "Check range")
-    (map Just expectedRanges)
-    (map (RDF.range . RDF.location) diagnostic)
+expectedRanges :: [RDF.Range]
+expectedRanges =
+  [ RDF.mkRange (10, 13) (10, 19),
+    RDF.mkRange (18, 13) (18, 22),
+    RDF.mkRange (22, 18) (22, 20),
+    RDF.mkRange (29, 14) (29, 16),
+    RDF.mkRange (31, 17) (31, 19),
+    RDF.mkRange (38, 14) (38, 16),
+    RDF.mkRange (39, 43) (39, 45),
+    RDF.mkRange (40, 17) (40, 19),
+    RDF.mkRange (41, 46) (41, 48),
+    RDF.mkRange (42, 21) (42, 23),
+    RDF.mkRange (51, 18) (51, 20),
+    RDF.mkRange (59, 18) (59, 20)
+  ]
