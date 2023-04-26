2d9f7ca5f9e6e455d85dca0f29c09a973c09168c
Christian Oehlers <averodas@mailbox.org>
2023-04-26 13:48:13 +0200

    Planning

--- src/Language/Java/Rules.hs
+++ src/Language/Java/Rules.hs
@@ -12,6 +12,7 @@ import qualified Language.Java.Rules.NoNullPointerExceptionsForControl as NoNull
 import qualified Language.Java.Rules.ParameterNumber as ParameterNumber
 import qualified Language.Java.Rules.PreferExpressions as PreferExpressions
 import qualified Language.Java.Rules.ProhibitAnnotations as ProhibitAnnotations
+import qualified Language.Java.Rules.ProhibitMyIdentPrefix as ProhibitMyIdentPrefix
 import qualified Language.Java.Rules.ReduceScope as ReduceScope
 import qualified Language.Java.Rules.RedundantModifiers as RedundantModifiers
 import qualified Language.Java.Rules.UseAssignOp as UseAssignOp
@@ -43,7 +44,8 @@ checks =
     DeclarationOrder.check,
     UseIncrementDecrementOperator.check,
     ProhibitAnnotations.check annotationswhitelist,
-    UseJavaArrayTypeStyle.check
+    UseJavaArrayTypeStyle.check,
+    ProhibitMyIdentPrefix.check
   ]
 
 checkAll :: CompilationUnit -> FilePath -> [RDF.Diagnostic]
---
+++ test/java/ProhibitMyIdentPrefix.java
@@ -0,0 +1,19 @@
+package my.huge.pckage.de;
+
+public class myClass {
+    String myString = "myStringy";
+    String ohneMy = "myString";
+    String mYear = "middleYear";
+
+    static final Int MY_YEAR = 1;
+
+    enum MyEnum {
+        myOne, myTwo, myThree
+    }
+
+
+    public myClass(){}
+
+    public void myMethod(){}
+
+}
