b3745c8f854a85e445de71d414fb9076bca09d5a
Mario Köhler <mario.koehler90@gmail.com>
2023-05-09 12:02:25 +0200

    Update code to use Located sourceSpan function

--- src/Language/Java/Rules/ProhibitAnnotations.hs
+++ src/Language/Java/Rules/ProhibitAnnotations.hs
@@ -3,7 +3,7 @@ module Language.Java.Rules.ProhibitAnnotations where
 import Control.Monad (MonadPlus (..))
 import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.Pretty (prettyPrint)
-import Language.Java.SourceSpan (SourceSpan)
+import Language.Java.SourceSpan (sourceSpan)
 import Language.Java.Syntax
 import qualified RDF
 
@@ -17,15 +17,10 @@ annotationName (NormalAnnotation _ name _) = name
 annotationName (SingleElementAnnotation _ name _) = name
 annotationName (MarkerAnnotation _ name) = name
 
-annotationSourceSpan :: Annotation -> SourceSpan
-annotationSourceSpan (NormalAnnotation sourcespan _ _) = sourcespan
-annotationSourceSpan (SingleElementAnnotation sourcespan _ _) = sourcespan
-annotationSourceSpan (MarkerAnnotation sourcespan _) = sourcespan
-
 checkAnnotation :: [String] -> Annotation -> FilePath -> [RDF.Diagnostic]
 checkAnnotation whitelist annotation path =
   let name = prettyPrint (annotationName annotation)
-      sourcespan = annotationSourceSpan annotation
+      sourcespan = sourceSpan annotation
    in if name `elem` whitelist
         then mzero
         else return (RDF.rangeDiagnostic "Language.Java.Rules.ProhibitAnnotations" ("Nicht erlaubte Annotation gefunden: " ++ name) sourcespan path)
