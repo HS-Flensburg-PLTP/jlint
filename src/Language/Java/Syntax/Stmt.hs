33d65280058dd7bfde827170fec00eac0c9c376d
Mario Köhler <91310869+mario-koehler@users.noreply.github.com>
2023-05-16 09:26:22 +0200

    Use sourceSpan for TypeDecl (#314)

--- src/Language/Java/Syntax/Stmt.hs
+++ src/Language/Java/Syntax/Stmt.hs
@@ -1,6 +1,5 @@
-module Language.Java.Syntax.Stmt (hasNoSideEffect, sourceSpan) where
+module Language.Java.Syntax.Stmt (hasNoSideEffect) where
 
-import Language.Java.SourceSpan (SourceSpan, dummySourceSpan)
 import Language.Java.Syntax (Stmt (..))
 
 -- Identifies statements that do not cause side effects concerning class attributes
@@ -23,16 +22,3 @@ hasNoSideEffect (Synchronized {}) = False
 hasNoSideEffect (Throw {}) = False
 hasNoSideEffect (Try {}) = False
 hasNoSideEffect (Labeled _ stmt) = hasNoSideEffect stmt
-
-sourceSpan :: Stmt -> SourceSpan
-sourceSpan (IfThen span _ _) = span
-sourceSpan (IfThenElse span _ _ _) = span
-sourceSpan (While span _ _) = span
-sourceSpan (BasicFor span _ _ _ _) = span
-sourceSpan (EnhancedFor span _ _ _ _ _) = span
-sourceSpan (ExpStmt span _) = span
-sourceSpan (Do span _ _) = span
-sourceSpan (Break span _) = span
-sourceSpan (Return span _) = span
-sourceSpan (Try span _ _ _ _) = span
-sourceSpan _ = dummySourceSpan
