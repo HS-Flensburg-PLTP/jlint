0ad5b30f1ab6eabf9e8b10d999db22de35b159ed
Joscha Konstantin Groth <joscha.groth@stud.hs-flensburg.de>
2023-05-31 15:01:14 +0200

    Write test java

--- test/java/ProhibitGermanNames.java
+++ test/java/ProhibitGermanNames.java
@@ -1,10 +1,14 @@
-class EinSuperDeutschesWort {
+class EineSuperDeutscheKlasse {
     myString testString = "Ahhhhh";
     VarDecl myRunsVar = 1;
 
+    public EineSuperDeutscheKlasse() {
+
+    }
+
     public void MyFunction() {
         for (int Maus = 0; maus < 10; maus++) {
-
+            String meineZeichenfolge = "Hier";
         }
     }
 }
