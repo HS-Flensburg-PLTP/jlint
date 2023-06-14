d75f4cd6353c7ec5044fe1ac94191576c6695105
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2023-06-14 16:49:33 +0200

    Remove old code

--- src/Language/Java/Rules/CheckNonFinalMethodParameters.hs
+++ src/Language/Java/Rules/CheckNonFinalMethodParameters.hs
@@ -8,25 +8,6 @@ import Language.Java.Syntax.Ident as Ident
 import Language.Java.Syntax.VarDecl as VarDecl
 import qualified RDF
 
-{- Alte Regel
-check :: CompilationUnit -> FilePath -> [RDF.Diagnostic]
-check cUnit path = do
-  formalParam <- extractMethodParameters cUnit
-  checkFormalParamList formalParam path
-
-checkFormalParamList :: (String, [FormalParam]) -> FilePath -> [RDF.Diagnostic]
-checkFormalParamList (_, formalParams) path = concatMap checkFormalParam formalParams
-  where
-    checkFormalParam (FormalParam _ modifier _ _ varid) =
-      [ RDF.rangeDiagnostic
-          "Language.Java.Rules.CheckNonFinalMethodAttributes"
-          (extractVarName varid ++ " is not declared as Final")
-          dummySourceSpan
-          path
-        | none (eq IgnoreSourceSpan Final) modifier
-      ]
--}
-
 check :: CompilationUnit -> FilePath -> [RDF.Diagnostic]
 check cUnit path = do
   FormalParam span modifier _ _ varid <- universeBi cUnit
