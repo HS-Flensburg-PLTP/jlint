fc97a3e5e8e4746535cd1f4e78a54305cb99a1b2
Joscha Konstantin Groth <joscha.groth@stud.hs-flensburg.de>
2023-06-15 14:45:29 +0200

    Improve tests

--- test/java/ProhibitGermanNames.java
+++ test/java/ProhibitGermanNames.java
@@ -1,14 +1,15 @@
 class EineSuperDeutscheKlasse {
-    String testString = "Ahhhhh";
-    int myRunsVar = 1;
     private final int WIRD_DAS_HIER_AUCH_GEFUNDEN = 1337;
-    private final String ANANAS = "ENDE";
+    private final String ANANAS = "PIZZA";
 
     public EineSuperDeutscheKlasse() {
         String wuerfel = "Würfel";
     }
 
     public void MyFunction() {
+        String testString = "Ahhhhh";
+        int myRunsVar = 1;
+
         for (int maus = 0; maus < 10; maus++) {
             String meineZeichenfolge = "Hier";
         }
