12f15951cb703e5f82aa8a68a3f8f8597fc8cd66
Joscha Konstantin Groth <joscha.groth@stud.hs-flensburg.de>
2023-06-15 14:16:32 +0200

    Use idents full sourceSpan

--- src/Language/Java/Rules/ProhibitGermanNames.hs
+++ src/Language/Java/Rules/ProhibitGermanNames.hs
@@ -7,6 +7,7 @@ module Language.Java.Rules.ProhibitGermanNames where
 import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.SourceSpan
 import Language.Java.Syntax
+import RDF (Diagnostic (source))
 import qualified RDF
 import System.Process
 import Text.RE.TDFA.String
@@ -60,20 +61,10 @@ checkAllMatches matches path = do
 
 splitIdent :: Ident -> [(String, SourceSpan)]
 splitIdent (Ident sourceSpan ident) =
-  calculateSourceSpans
+  concatMap
+    ( \match -> case matchedText match of
+        Nothing -> []
+        Just string ->
+          [(string, sourceSpan)]
+    )
     (allMatches (ident *=~ [re|([a-z]|[A-Z])[a-z]+|([A-Z]{2,})+|]))
-    sourceSpan
-
-calculateSourceSpans :: [Match String] -> SourceSpan -> [(String, SourceSpan)]
-calculateSourceSpans [] _ = []
-calculateSourceSpans (match : matches) (Location fileStart lineStart colStart, Location fileEnd lineEnd colEnd) = case matchedText match of
-  Nothing -> []
-  Just string ->
-    ( string,
-      ( Location fileStart lineStart colStart,
-        Location fileEnd lineEnd (colStart + length string - 1)
-      )
-    ) :
-    calculateSourceSpans
-      matches
-      (Location fileStart lineStart (colStart + length string), Location fileEnd lineEnd colEnd)
