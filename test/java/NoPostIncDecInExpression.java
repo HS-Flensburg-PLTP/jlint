b5b2b4aab9e6216ccffcc0319af5bfcf55b5b4be
Christian Oehlers <averodas@mailbox.org>
2023-06-16 13:35:58 +0200

    Add Test Cases

--- test/java/NoPostIncDecInExpression.java
+++ test/java/NoPostIncDecInExpression.java
@@ -4,8 +4,24 @@ class FooBar {
 
     public Foobar () {
         int i = (i--)++;
+        int a = 1;
+        int[] array = new int[4];
+
+
+        array[a--] = 4
+
         for (int j = i++; j < 10; j++) {
 
         }
+        if (i++ == a--) {
+            return i++ != a--;
+        }
+        Funct(a--);
+        a++;
+
+    }
+
+    private void Funct (int b) {
+        b = a++;
     }
 }
