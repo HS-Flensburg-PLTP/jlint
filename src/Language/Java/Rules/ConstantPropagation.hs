bce1e456892c19735baf60966fa95e5a71a2fd89
Philip Resech <philip.resech@stud.hs-flensburg.de>
2023-06-18 22:25:57 +0200

    Remove string Parameter on checkInnerStmt and use sourceSpan of innerName instead

--- src/Language/Java/Rules/ConstantPropagation.hs
+++ src/Language/Java/Rules/ConstantPropagation.hs
@@ -5,7 +5,6 @@ module Language.Java.Rules.ConstantPropagation (check) where
 import Control.Monad (MonadPlus (..))
 import Data.Generics.Uniplate.Data (universeBi)
 import Language.Java.Pretty (prettyPrint)
-import Language.Java.SourceSpan
 import Language.Java.Syntax
 import qualified RDF
 
@@ -14,22 +13,22 @@ check cUnit path = do
   stmt <- universeBi cUnit
   checkStmt stmt
   where
-    checkStmt (IfThen span (BinOp (ExpName outerName) Equal (Lit lit)) thenStmt) = checkInnerStmt span path thenStmt outerName lit "then"
-    checkStmt (IfThen span (BinOp (Lit lit) Equal (ExpName outerName)) thenStmt) = checkInnerStmt span path thenStmt outerName lit "then"
-    checkStmt (IfThenElse span (BinOp (ExpName outerName) NotEq (Lit lit)) _ elseStmt) = checkInnerStmt span path elseStmt outerName lit "else"
-    checkStmt (IfThenElse span (BinOp (Lit lit) NotEq (ExpName outerName)) _ elseStmt) = checkInnerStmt span path elseStmt outerName lit "else"
+    checkStmt (IfThen _ (BinOp (ExpName outerName) Equal (Lit lit)) thenStmt) = checkInnerStmt path thenStmt outerName lit
+    checkStmt (IfThen _ (BinOp (Lit lit) Equal (ExpName outerName)) thenStmt) = checkInnerStmt path thenStmt outerName lit
+    checkStmt (IfThenElse _ (BinOp (ExpName outerName) NotEq (Lit lit)) _ elseStmt) = checkInnerStmt path elseStmt outerName lit
+    checkStmt (IfThenElse _ (BinOp (Lit lit) NotEq (ExpName outerName)) _ elseStmt) = checkInnerStmt path elseStmt outerName lit
     checkStmt _ = mzero
 
-checkInnerStmt :: SourceSpan -> FilePath -> Stmt Parsed -> Name -> Literal -> String -> [RDF.Diagnostic]
-checkInnerStmt span path thenStmt outerName lit ifcase = do
+checkInnerStmt :: FilePath -> Stmt Parsed -> Name -> Literal -> [RDF.Diagnostic]
+checkInnerStmt path thenStmt outerName lit = do
   exp :: Exp Parsed <- universeBi thenStmt
   case exp of
-    (ExpName innerName) ->
+    (ExpName innerName@(Name span _)) ->
       if eq IgnoreSourceSpan outerName innerName
         then
           [ RDF.rangeDiagnostic
               "Language.Java.Rules.ConstantPropagation"
-              ("Anstatt im " ++ ifcase ++ "-Fall erneut `" ++ prettyPrint outerName ++ "` zu verwenden, kann hier direkt das Literal `" ++ prettyPrint lit ++ "` verwendet werden.")
+              ("Anstatt erneut `" ++ prettyPrint outerName ++ "` zu verwenden, kann hier direkt das Literal `" ++ prettyPrint lit ++ "` verwendet werden.")
               span
               path
           ]
