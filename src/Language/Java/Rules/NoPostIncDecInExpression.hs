ea4c90117e9ded23892a7133cd95bc2b689e250e
Christian Oehlers <averodas@mailbox.org>
2023-06-22 13:16:28 +0200

    use mzero, use list monad instead of ugly filter

--- src/Language/Java/Rules/NoPostIncDecInExpression.hs
+++ src/Language/Java/Rules/NoPostIncDecInExpression.hs
@@ -1,45 +1,46 @@
 module Language.Java.Rules.NoPostIncDecInExpression where
 
+import Control.Monad (MonadPlus (..))
 import Data.Generics.Uniplate.Data (universeBi)
+import Language.Java.SourceSpan (sourceSpan)
 import Language.Java.Syntax
 import qualified Markdown
 import qualified RDF
 
 check :: CompilationUnit Parsed -> FilePath -> [RDF.Diagnostic]
 check cUnit path = do
-  let statements = universeBi cUnit
-  let allowedExpressions = concatMap checkStatement statements
-  let allPostIncDec = universeBi cUnit
-  concatMap (`createDiagnostic` path) (filter (\e -> not (any (comparePostIncDec e) allowedExpressions)) allPostIncDec)
-
-comparePostIncDec :: Exp Parsed -> Exp Parsed -> Bool
-comparePostIncDec (PostIncrement sp1 _) (PostIncrement sp2 _) = sp1 == sp2
-comparePostIncDec (PostDecrement sp1 _) (PostDecrement sp2 _) = sp1 == sp2
-comparePostIncDec _ _ = False
+  statement <- universeBi cUnit
+  let allowedExpressions = checkStatement statement
+  postIncDec <- universeBi cUnit
+  if any (\allowExp -> sourceSpan postIncDec == sourceSpan allowExp) allowedExpressions
+    then mzero
+    else createDiagnostic postIncDec path
 
 checkStatement :: Stmt Parsed -> [Exp Parsed]
 checkStatement (ExpStmt _ expression) = checkExpression expression
 checkStatement (BasicFor _ _ _ (Just expressions) _) = concatMap checkExpression expressions
-checkStatement _ = []
+checkStatement _ = mzero
 
 checkExpression :: Exp Parsed -> [Exp Parsed]
-checkExpression (PostIncrement sourceSpan exp) = [PostIncrement sourceSpan exp]
-checkExpression (PostDecrement sourceSpan exp) = [PostDecrement sourceSpan exp]
-checkExpression _ = []
+checkExpression (PostIncrement sourceSpan exp) = return (PostIncrement sourceSpan exp)
+checkExpression (PostDecrement sourceSpan exp) = return (PostDecrement sourceSpan exp)
+checkExpression _ = mzero
 
 createDiagnostic :: Exp Parsed -> FilePath -> [RDF.Diagnostic]
 createDiagnostic (PostIncrement sourceSpan _) path =
-  [ RDF.rangeDiagnostic
-      "Language.Java.Rules.NoPostIncDecInExpression"
-      ("Der Operator " ++ Markdown.code "++" ++ " ist an dieser Stelle nicht erlaubt, da es zu unleserlichem sowie unverständlichem Code führt.")
-      sourceSpan
-      path
-  ]
+  return
+    ( RDF.rangeDiagnostic
+        "Language.Java.Rules.NoPostIncDecInExpression"
+        ("Der Operator " ++ Markdown.code "++" ++ " ist an dieser Stelle nicht erlaubt, da es zu unleserlichem sowie unverständlichem Code führt.")
+        sourceSpan
+        path
+    )
 createDiagnostic (PostDecrement sourceSpan _) path =
-  [ RDF.rangeDiagnostic
-      "Language.Java.Rules.NoPostIncDecInExpression"
-      ("Der Operator " ++ Markdown.code "--" ++ " ist an dieser Stelle nicht erlaubt, da es zu unleserlichem sowie unverständlichem Code führt.")
-      sourceSpan
-      path
-  ]
-createDiagnostic _ _ = []
+  return
+    ( RDF.rangeDiagnostic
+        "Language.Java.Rules.NoPostIncDecInExpression"
+        ("Der Operator " ++ Markdown.code "--" ++ " ist an dieser Stelle nicht erlaubt, da es zu unleserlichem sowie unverständlichem Code führt.")
+        sourceSpan
+        path
+    )
+createDiagnostic _ _ = mzero
