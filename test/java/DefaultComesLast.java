b3b87d7752198784646a154661bf1297ee0c7e7f
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2023-07-07 10:49:19 +0200

    Refactor DefaultComesLast (#329)

--- test/java/DefaultComesLast.java
+++ test/java/DefaultComesLast.java
@@ -11,4 +11,51 @@ class Test {
                 break;
         }
     }
+    
+    public void foo() {
+    switch(i){
+        default:
+            break;
+        case 2 :
+            a = "b";
+            break;
+        case 3 :
+            a = "c";
+        }
+    }
+
+    public void bar() {
+        switch (i) {
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }
+
+    public void cheese() {
+        switch (i) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void mouse() {
+        switch (i) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+            default:
+                break;
+        }
+    }
+    
 }
+
