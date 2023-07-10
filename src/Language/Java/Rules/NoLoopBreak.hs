5375f9e199326e73974c21e6043bfd3aa8f6ab97
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2023-07-10 16:18:24 +0200

    Refactor Rule and change tests

--- src/Language/Java/Rules/NoLoopBreak.hs
+++ src/Language/Java/Rules/NoLoopBreak.hs
@@ -1,12 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Language.Java.Rules.NoLoopBreak (check) where
 
 import Control.Monad.Reader
 import Data.Generics.Uniplate.Data (universeBi)
-import Language.Java.AST (extractMethods)
-import Language.Java.SourceSpan (dummySourceSpan)
 import Language.Java.Syntax
 import qualified RDF
 
+{-
 check :: CompilationUnit Parsed -> FilePath -> [RDF.Diagnostic]
 check cUnit path = do
   methods <- extractMethods cUnit
@@ -47,3 +48,62 @@ extractLoopBody ((BlockStmt _ block) : xs) = do
 extractLoopBody ((LocalClass _) : xs) = extractLoopBody xs
 extractLoopBody ((LocalVars {}) : xs) = extractLoopBody xs
 extractLoopBody [] = return []
+-}
+
+check :: CompilationUnit Parsed -> FilePath -> [RDF.Diagnostic]
+check cUnit path = do
+  stmt <- universeBi cUnit
+  checkStatement stmt path
+
+checkStatement :: Stmt Parsed -> FilePath -> [RDF.Diagnostic]
+checkStatement (While _ _ stmt) path = checkReturn stmt path ++ checkBreak stmt path
+checkStatement (Do _ stmt _) path = checkReturn stmt path ++ checkBreak stmt path
+checkStatement (BasicFor _ _ _ _ stmt) path = checkReturn stmt path ++ checkBreak stmt path
+checkStatement (EnhancedFor _ _ _ _ _ stmt) path = checkReturn stmt path ++ checkBreak stmt path
+checkStatement _ _ = mzero
+
+checkReturn :: Stmt Parsed -> FilePath -> [RDF.Diagnostic]
+checkReturn stmt path = do
+  let allReturns :: [Stmt Parsed] = universeBi stmt
+  stmt <- listDifference allReturns (checkLoops stmt)
+  returnMsg path stmt
+
+checkBreak :: Stmt Parsed -> FilePath -> [RDF.Diagnostic]
+checkBreak stmt path = do
+  let allBreaks :: [Stmt Parsed] = universeBi stmt
+  stmt <- listDifference allBreaks (checkSwitchBlock stmt ++ checkLoops stmt)
+  breakMsg path stmt
+
+checkLoops :: Stmt Parsed -> [Stmt Parsed]
+checkLoops stmt = do
+  loop <- universeBi stmt
+  loopBody <- extractLoopBody loop
+  universeBi loopBody
+
+extractLoopBody :: Stmt Parsed -> [Stmt Parsed]
+extractLoopBody (While _ _ stmt) = return stmt
+extractLoopBody (Do _ stmt _) = return stmt
+extractLoopBody (BasicFor _ _ _ _ stmt) = return stmt
+extractLoopBody (EnhancedFor _ _ _ _ _ stmt) = return stmt
+extractLoopBody _ = mzero
+
+checkSwitchBlock :: Stmt Parsed -> [Stmt Parsed]
+checkSwitchBlock stmt = do
+  Switch _ _ blocks :: Stmt Parsed <- universeBi stmt
+  let switchBreaks :: [Stmt Parsed] = universeBi blocks
+  switchBreaks
+
+listDifference :: [Stmt Parsed] -> [Stmt Parsed] -> [Stmt Parsed]
+listDifference completeList excludedList = do
+  elem <- completeList
+  if any (eq IncludeSourceSpan elem) excludedList
+    then mzero
+    else return elem
+
+breakMsg :: FilePath -> Stmt Parsed -> [RDF.Diagnostic]
+breakMsg path (Break span _) = return (RDF.rangeDiagnostic "Language.Java.Rules.NoLoopBreak" "Eine Schleife sollte nicht mit einem Break beendet werden." span path)
+breakMsg _ _ = mzero
+
+returnMsg :: FilePath -> Stmt Parsed -> [RDF.Diagnostic]
+returnMsg path (Return span _) = return (RDF.rangeDiagnostic "Language.Java.Rules.NoLoopBreak" "Eine Schleife sollte nicht mit einem Return beendet werden." span path)
+returnMsg _ _ = mzero
