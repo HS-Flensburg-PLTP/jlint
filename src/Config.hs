021c46324fec157b3443906e20f35fc5f0b5f544
Philip Resech <philip.resech@stud.hs-flensburg.de>
2023-08-10 22:58:56 +0200

    Removed broken rules and rename old rules

--- src/Config.hs
+++ src/Config.hs
@@ -22,10 +22,8 @@ data Rule
   | InitializeVariables
   | MethodInvNumber {called :: String, limited :: String, maxInv :: Int}
   | ModifiedControlVariable
-  | NamingConventions
   | NeedBraces
   | NoCasts {whitelist :: [String]}
-  | NoLoopBreak
   | NoNullPointerExceptionsForControl
   | ParameterNumber {max :: Maybe Int}
   | PreferExpressions
@@ -33,9 +31,6 @@ data Rule
   | ProhibitMyIdentPrefix
   | ReduceScope
   | RedundantModifiers
-  | SameExecutionsInIf
-  | SimplifyBooleanReturn
-  | UnusedLocalVariable
   | UseAssignOp
   | UseElse
   | UseIncrementDecrementOperator
@@ -60,10 +55,8 @@ instance FromJSON Rule where
       "InitializeVariables" -> pure InitializeVariables
       "MethodInvNumber" -> parseMethodInvNumber obj
       "ModifiedControlVariable" -> pure ModifiedControlVariable
-      "NamingConventions" -> pure NamingConventions
       "NeedBraces" -> pure NeedBraces
       "NoCasts" -> parseNoCasts obj
-      "NoLoopBreak" -> pure NoLoopBreak
       "NoNullPointerExceptionsForControl" -> pure NoNullPointerExceptionsForControl
       "ParameterNumber" -> parseParameterNumber obj
       "PreferExpressions" -> pure PreferExpressions
@@ -71,9 +64,6 @@ instance FromJSON Rule where
       "ProhibitMyIdentPrefix" -> pure ProhibitMyIdentPrefix
       "ReduceScope" -> pure ReduceScope
       "RedundantModifiers" -> pure RedundantModifiers
-      "SameExecutionsInIf" -> pure SameExecutionsInIf
-      "SimplifyBooleanReturn" -> pure SimplifyBooleanReturn
-      "UnusedLocalVariable" -> pure UnusedLocalVariable
       "UseAssignOp" -> pure UseAssignOp
       "UseElse" -> pure UseElse
       "UseIncrementDecrementOperator" -> pure UseIncrementDecrementOperator
