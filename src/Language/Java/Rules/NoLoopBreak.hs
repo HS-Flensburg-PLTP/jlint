7cb74ba40f0cefd1c3803a1391a637c466bc2ff9
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2023-08-10 19:12:34 +0200

    Add review

--- src/Language/Java/Rules/NoLoopBreak.hs
+++ src/Language/Java/Rules/NoLoopBreak.hs
@@ -11,34 +11,30 @@ import qualified RDF
 check :: CompilationUnit Parsed -> FilePath -> [RDF.Diagnostic]
 check cUnit path = do
   stmt <- universeBi cUnit
-  checkStatement stmt path
-
-checkStatement :: Stmt Parsed -> FilePath -> [RDF.Diagnostic]
-checkStatement stmt path =
   case extractLoopBody stmt of
     Just loopBody -> checkReturn loopBody path ++ checkBreak loopBody path
     Nothing -> mzero
 
 checkReturn :: Stmt Parsed -> FilePath -> [RDF.Diagnostic]
 checkReturn stmt path = do
-  stmt <- listDifference (universeBi stmt) (checkLoops stmt)
-  filterReturn stmt
-  where
-    filterReturn (Return span _) = returnMsg path span
-    filterReturn _ = mzero
+  (Return span _) <- universeBi stmt // checkLoops stmt
+  returnMsg path span
 
 checkBreak :: Stmt Parsed -> FilePath -> [RDF.Diagnostic]
 checkBreak stmt path = do
-  stmt <- listDifference (universeBi stmt) (checkSwitchBlock stmt ++ checkLoops stmt)
-  filterBreak stmt
-  where
-    filterBreak (Break span _) = breakMsg path span
-    filterBreak _ = mzero
+  (Break span _) <- universeBi stmt // (checkLoops stmt ++ checkSwitchBlock stmt)
+  breakMsg path span
 
 checkLoops :: Stmt Parsed -> [Stmt Parsed]
 checkLoops stmt = do
   loop <- universeBi stmt
   maybe mzero universeBi (extractLoopBody loop)
+
+checkSwitchBlock :: Stmt Parsed -> [Stmt Parsed]
+checkSwitchBlock stmt = do
+  Switch _ _ _ blocks :: Stmt Parsed <- universeBi stmt
+  let switchBreaks :: [Stmt Parsed] = universeBi blocks
+  switchBreaks
 
 extractLoopBody :: Stmt Parsed -> Maybe (Stmt Parsed)
 extractLoopBody (While _ _ stmt) = Just stmt
@@ -47,13 +43,8 @@ extractLoopBody (BasicFor _ _ _ _ stmt) = Just stmt
 extractLoopBody (EnhancedFor _ _ _ _ _ stmt) = Just stmt
 extractLoopBody _ = Nothing
 
-checkSwitchBlock :: Stmt Parsed -> [Stmt Parsed]
-checkSwitchBlock stmt = do
-  Switch _ _ blocks :: Stmt Parsed <- universeBi stmt
-  universeBi blocks
-
-listDifference :: [Stmt Parsed] -> [Stmt Parsed] -> [Stmt Parsed]
-listDifference completeList excludedList = do
+(//) :: [Stmt Parsed] -> [Stmt Parsed] -> [Stmt Parsed]
+completeList // excludedList = do
   elem <- completeList
   if any (eq IncludeSourceSpan elem) excludedList
     then mzero
