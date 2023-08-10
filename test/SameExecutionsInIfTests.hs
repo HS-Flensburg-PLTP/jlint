f4fc3f59aacc4e59c701a166b01c6bcabc54a99a
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2023-08-10 18:44:16 +0200

    Rework and fix rule SameExecutionsInIf
    * Refactor rule and improve testing
    
    * Add missing cases
    
    * Improve cases
    
    * Remove not null
    
    * Add guard before merge

--- test/SameExecutionsInIfTests.hs
+++ test/SameExecutionsInIfTests.hs
@@ -14,8 +14,14 @@ tests =
 
 expectedRanges :: [RDF.Range]
 expectedRanges =
-  [ RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1)
+  [ RDF.mkRange (4, 9) (9, 10),
+    RDF.mkRange (15, 16) (19, 10),
+    RDF.mkRange (23, 9) (29, 10),
+    RDF.mkRange (31, 9) (41, 10),
+    RDF.mkRange (43, 9) (53, 10),
+    RDF.mkRange (79, 9) (83, 22),
+    RDF.mkRange (84, 9) (88, 22),
+    RDF.mkRange (98, 9) (101, 10),
+    RDF.mkRange (106, 9) (107, 20),
+    RDF.mkRange (111, 9) (113, 10)
   ]
