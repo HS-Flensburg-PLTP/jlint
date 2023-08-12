feadf7686b1f65bdd28d64f365b09f0475ae20ef
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2023-08-12 20:26:06 +0200

    Rework and improve rule NoLoopBreak
    * Refactor Rule and change tests
    
    * Remove old code
    
    * Add local functions
    
    * Add review
    
    * Add review
    
    * Remove unused code
    
    * Add review
    
    * Clean up and simplify code
    
    ---------
    
    Co-authored-by: Mario Köhler <mario.koehler90@gmail.com>

--- test/NoLoopBreakTests.hs
+++ test/NoLoopBreakTests.hs
@@ -14,16 +14,18 @@ tests =
 
 expectedRanges :: [RDF.Range]
 expectedRanges =
-  [ RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1),
-    RDF.mkRange (1, 1) (1, 1)
+  [ RDF.mkRange (9, 20) (9, 26),
+    RDF.mkRange (20, 17) (20, 23),
+    RDF.mkRange (32, 13) (32, 19),
+    RDF.mkRange (37, 13) (37, 19),
+    RDF.mkRange (42, 13) (42, 19),
+    RDF.mkRange (47, 13) (47, 19),
+    RDF.mkRange (53, 17) (53, 23),
+    RDF.mkRange (60, 17) (60, 23),
+    RDF.mkRange (68, 13) (68, 20),
+    RDF.mkRange (73, 13) (73, 20),
+    RDF.mkRange (78, 13) (78, 20),
+    RDF.mkRange (83, 13) (83, 20),
+    RDF.mkRange (89, 17) (89, 24),
+    RDF.mkRange (96, 17) (96, 24)
   ]
