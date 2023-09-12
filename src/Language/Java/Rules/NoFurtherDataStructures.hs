4f7d3042616f358c93ba7d1eb5e1a3a4591b18ba
Jan Christiansen <jan.christiansen@hs-flensburg.de>
2023-09-12 14:04:54 +0200

    Fix argument of rule configuration

--- src/Language/Java/Rules/NoFurtherDataStructures.hs
+++ src/Language/Java/Rules/NoFurtherDataStructures.hs
@@ -4,15 +4,17 @@ module Language.Java.Rules.NoFurtherDataStructures where
 
 import Control.Monad (MonadPlus (..))
 import Data.Generics.Uniplate.Data (universeBi)
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NonEmpty
 import Language.Java.Syntax
 import qualified Language.Java.Syntax.Exp.Extra as Exp.Extra
 import qualified Language.Java.Syntax.Ident as Ident
 import qualified RDF
 
-check :: [String] -> CompilationUnit Parsed -> FilePath -> [RDF.Diagnostic]
+check :: NonEmpty String -> CompilationUnit Parsed -> FilePath -> [RDF.Diagnostic]
 check methodNames cUnit path = do
   MethodDecl span _ _ _ ident _ _ _ body <- universeBi cUnit :: [MemberDecl Parsed]
-  methodName <- methodNames
+  methodName <- NonEmpty.toList methodNames
   if Ident.name ident == methodName && any Exp.Extra.isDataCreation (universeBi body :: [Exp Parsed])
     then
       return
