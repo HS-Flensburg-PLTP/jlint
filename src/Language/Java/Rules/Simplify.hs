3e2f3daba5a1c06a54053e26294ebb2d4c0d0cb8
Jan Christiansen <jan.christiansen@hs-flensburg.de>
2023-12-07 13:28:01 +0100

    Disable Simplify rule

--- src/Language/Java/Rules/Simplify.hs
+++ src/Language/Java/Rules/Simplify.hs
@@ -9,32 +9,34 @@ import qualified Markdown
 import qualified RDF
 
 check :: CompilationUnit Parsed -> FilePath -> [RDF.Diagnostic]
-check cUnit path = mapMaybe checkExp (universeBi cUnit)
-  where
-    checkExp :: Exp Parsed -> Maybe RDF.Diagnostic
-    checkExp (BinOp span exp1 Equal exp2)
-      | callsCompareTo exp1 && isZero exp2 || isZero exp1 && callsCompareTo exp2 =
-          return
-            ( RDF.rangeDiagnostic
-                "Language.Java.Rules.Simplify"
-                [ "Statt das Ergebnis von",
-                  Markdown.code "compareTo",
-                  "mit",
-                  Markdown.code "0",
-                  "zu vergleichen, sollte die Methode",
-                  Markdown.code "equals",
-                  "verwendet werden."
-                ]
-                span
-                path
-            )
-      | otherwise = mzero
-    checkExp _ = mzero
+check cUnit path = mzero
 
-callsCompareTo :: Exp p -> Bool
-callsCompareTo (MethodInv (PrimaryMethodCall _ _ _ ident _)) = Ident.name ident == "compareTo"
-callsCompareTo _ = False
+-- mapMaybe checkExp (universeBi cUnit)
+-- where
+--   checkExp :: Exp Parsed -> Maybe RDF.Diagnostic
+--   checkExp (BinOp span exp1 Equal exp2)
+--     | callsCompareTo exp1 && isZero exp2 || isZero exp1 && callsCompareTo exp2 =
+--         return
+--           ( RDF.rangeDiagnostic
+--               "Language.Java.Rules.Simplify"
+--               [ "Statt das Ergebnis von",
+--                 Markdown.code "compareTo",
+--                 "mit",
+--                 Markdown.code "0",
+--                 "zu vergleichen, sollte die Methode",
+--                 Markdown.code "equals",
+--                 "verwendet werden."
+--               ]
+--               span
+--               path
+--           )
+--     | otherwise = mzero
+--   checkExp _ = mzero
 
-isZero :: Exp p -> Bool
-isZero (Lit (Int _ 0)) = True
-isZero _ = False
+-- callsCompareTo :: Exp p -> Bool
+-- callsCompareTo (MethodInv (PrimaryMethodCall _ _ _ ident _)) = Ident.name ident == "compareTo"
+-- callsCompareTo _ = False
+
+-- isZero :: Exp p -> Bool
+-- isZero (Lit (Int _ 0)) = True
+-- isZero _ = False
